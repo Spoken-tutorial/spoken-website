b964aeea2bb3f8eb8fa633a323260b18d26a2435
Sanmugasundaram K <k.sanmugam2@gmail.com>
2014-05-28 10:32:59 +0530

    header included

--- cms/templatetags/cmsdata.py
+++ cms/templatetags/cmsdata.py
@@ -4,43 +4,54 @@ from cms.models import Block, Nav, SubNav
 register = template.Library()
 
 def get_cms_sidebar():
-	block = Block.objects.all().filter(block_location_id=3, visible__exact=1).order_by('position')
-	context = {
-		'blocks': block
-	}
+    block = Block.objects.all().filter(block_location_id=3, visible__exact=1).order_by('position')
+    context = {
+        'blocks': block
+    }
 
-	return context
+    return context
 
 def get_cms_nav():
-	nav = Nav.objects.all().filter(visible__exact=1).order_by('position')
-	context = {
-		'navs': nav
-	}
+    nav = Nav.objects.all().filter(visible__exact=1).order_by('position')
+    context = {
+        'navs': nav
+    }
 
-	return context
+    return context
 
 @register.filter
 def sort_by(queryset, order):
-	return queryset.filter(visible__exact=1).order_by('position')
+    return queryset.filter(visible__exact=1).order_by('position')
 
 def get_cms_footer():
-	block = Block.objects.all().filter(block_location_id=2, visible__exact=1).order_by('position')
-	context = {
-		'blocks': block
-	}
+    block = Block.objects.all().filter(block_location_id=2, visible__exact=1).order_by('position')
+    context = {
+        'blocks': block
+    }
 
-	return context
+    return context
 
 def get_cms_header():
-	block = Block.objects.all().filter(block_location_id=1, visible__exact=1).order_by('position')
-	context = {
-		'blocks': block
-	}
+    block = Block.objects.all().filter(block_location_id=1, visible__exact=1).order_by('position')
+    context = {
+        'blocks': block
+    }
 
-	return context
-
+    return context
+    
+def get_or_create_csrf_token(request):
+    from django.middleware import csrf
+    token = request.META.get('CSRF_COOKIE', None)
+    if token is None:
+        token = csrf._get_new_csrf_key()
+        request.META['CSRF_COOKIE'] = token
+    request.META['CSRF_COOKIE_USED'] = True
+    return token
+    
+    
 register.inclusion_tag('cms/templates/cmsnav.html')(get_cms_nav)
 register.inclusion_tag('cms/templates/cmsidebar.html')(get_cms_sidebar)
 register.inclusion_tag('cms/templates/cmsfooter.html')(get_cms_footer)
 register.inclusion_tag('cms/templates/cmsheader.html')(get_cms_header)
+register.filter('get_or_create_csrf_token', get_or_create_csrf_token)
 
--- spoken/settings.py
+++ spoken/settings.py
@@ -155,5 +155,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.media",
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
-    "django.contrib.messages.context_processors.messages"
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request"
 )
