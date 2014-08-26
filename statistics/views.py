482729e262219a38411bcccf6b86b1920cfae1a7
Jayaram <k.sanmugam2@gmail.com>
2014-08-26 18:35:41 +0530

    statistics page fixes

--- statistics/views.py
+++ statistics/views.py
@@ -2,7 +2,9 @@ from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.db.models import Count, Sum, Min
+from django.db.models import Q
 from events.models import *
+import datetime
 
 # Create your views here.
 def maphome(request):
@@ -16,9 +18,10 @@ def get_state_info(request, code):
     state = None
     try:
         state = State.objects.get(code = code)
-        academic_list = AcademicCenter.objects.filter(state = state).values_list('id')
+        #academic_list = AcademicCenter.objects.filter(state = state).values_list('id')
         resource_centers = AcademicCenter.objects.filter(state = state, resource_center = 1).count()
-        workshop_details = Training.objects.filter(academic_id__in = academic_list, status = 4).aggregate(Sum('participant_counts'), Count('id'), Min('trdate'))
+        #workshop_details = Training.objects.filter(academic_id__in = academic_list, status = 4).aggregate(Sum('participant_counts'), Count('id'), Min('trdate'))
+        workshop_details = Training.objects.filter(Q(status = 4) | (Q(training_type = 0) & Q(status__gt = 0) & Q(trdate__lte = datetime.date.today())), academic__state_id = state.id).aggregate(Sum('participant_counts'), Count('id'), Min('trdate'))
         context = {
             'state_name': state.name,
             'workshops': workshop_details['id__count'],
