efdf4d08db81c03f2f13397d7530112624451430
vishnukraj <vishnukraj007@gmail.com>
2014-11-10 12:41:38 +0530

    added new file to cron for deleting existing mp4 files and made some changes in mp4 creation

--- cron/create-missing-mp4-videos.py
+++ cron/create-missing-mp4-videos.py
@@ -2,22 +2,19 @@ import subprocess
 import MySQLdb
 import os
 import time
-from youtube_upload import *
 from config import *
 
-def convert_video(src_path, dst_path):
+def convert_tmp_video(src_path, dst_path):
     stdout = None
     stderr = None
-    """ffmpeg -i Hardware-requirement-to-install-Blender-English.ogv -acodec libfaac -ac 2 -ab 160k -vcodec libx264 -vpre fast -f mp4 Hardware-requirement-to-install-Blender-English.mp4"""
-    """ffmpeg -i Registration-of-an-account-for-online-train-ticket-booking-English.ogv -strict experimental -vcodec libx264 -acodec libfaac -vpre fast -f mp4 Registration-of-an-account-for-online-train-ticket-booking-English.mp4"""
+    """mffmpeg -i input-file.ogv -strict experimental -pix_fmt yuv420p -r 15 -f mp4 tmp.mp4"""
     process = subprocess.Popen(
         [
-            '/usr/bin/ffmpeg',
+            '/usr/bin/mffmpeg',
             '-i', src_path,
             '-strict', 'experimental',
-            '-vcodec', 'libx264',
-            '-acodec', 'libfaac',
-            '-vpre', 'fast',
+            '-pix_fmt', 'yuv420p',
+            '-r', '15',
             '-f', 'mp4',
             dst_path
         ],
@@ -27,14 +24,28 @@ def convert_video(src_path, dst_path):
     stdout, stderr = process.communicate()
     return stdout, stderr
 
-# creating youtube object
-youtube = Youtube(DEVELOPER_KEY)
-debug("Login to Youtube API: email='%s', password='%s'" %
-      (EMAIL, "*" * len(PASSWORD)))
-try:
-    youtube.login(EMAIL, PASSWORD)
-except gdata.service.BadAuthentication:
-    raise BadAuthentication("Authentication failed")
+def convert_video(src_path, dst_path):
+    stdout = None
+    stderr = None
+    """ffmpeg -i Hardware-requirement-to-install-Blender-English.ogv -acodec libfaac -ac 2 -ab 160k -vcodec libx264 -vpre fast -f mp4 Hardware-requirement-to-install-Blender-English.mp4"""
+    """ffmpeg -i Registration-of-an-account-for-online-train-ticket-booking-English.ogv -strict experimental -vcodec libx264 -acodec libfaac -vpre fast -f mp4 Registration-of-an-account-for-online-train-ticket-booking-English.mp4"""
+    """ffmpeg -i tmp.mp4 -strict experimental -vcodec libx264 -vpre default -f mp4 output.mp4"""
+    process = subprocess.Popen(
+        [
+            '/usr/bin/ffmpeg',
+            '-i', src_path,
+            '-strict', 'experimental',
+            '-vcodec', 'libx264',
+            '-vpre', 'default',
+            '-f', 'mp4',
+            dst_path
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
+    stdout, stderr = process.communicate()
+    return stdout, stderr
+
 
 db = MySQLdb.connect(host = DB_HOST, user = DB_USER, passwd = DB_PASS, \
     db = DB_NAME)
@@ -57,17 +68,24 @@ for row in rows:
     print row[5]
     ogv_video_path = MEDIA_ROOT + 'videos/' + str(row[8]) + '/' + str(row[1]) + '/' + row[5]
     mp4_video_path, file_extension = os.path.splitext(ogv_video_path)
+    mp4_tmp_video_path = mp4_video_path + '-tmp.mp4'
     mp4_video_path = mp4_video_path + '.mp4'
     if os.path.isfile(mp4_video_path):
         if not os.path.getsize(mp4_video_path):
             print 'Converting video...'
-            stdout, stderr = convert_video(ogv_video_path, mp4_video_path)
+            stdout, stderr = convert_tmp_video(ogv_video_path, mp4_tmp_video_path)
+            print 'Stdout: ', stdout
+            print 'Stderr: ', stderr
+            stdout, stderr = convert_video(mp4_tmp_video_path, mp4_video_path)
             print 'Stdout: ', stdout
             print 'Stderr: ', stderr
             error_log_file_head1.write(mp4_video_path + ', Conversion Error' + '\n')
     else:
         print 'Converting video...'
-        stdout, stderr = convert_video(ogv_video_path, mp4_video_path)
+        stdout, stderr = convert_tmp_video(ogv_video_path, mp4_tmp_video_path)
+        print 'Stdout: ', stdout
+        print 'Stderr: ', stderr
+        stdout, stderr = convert_video(mp4_tmp_video_path, mp4_video_path)
         print 'Stdout: ', stdout
         print 'Stderr: ', stderr
         error_log_file_head2.write(mp4_video_path + ', File not found' + '\n')
