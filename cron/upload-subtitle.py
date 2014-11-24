e481accc6032705d680747b46d9647b9263cd638
vishnukraj <vishnukraj007@gmail.com>
2014-11-24 16:13:27 +0530

    youtube subtitle upload script -- bug fix

--- cron/upload-subtitle.py
+++ cron/upload-subtitle.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
         db_cursor.execute("select id, name, code from creation_language \
             where id = %d", [int(row[1]),])
         language = db_cursor.fetchone()
-        video_title = str(row[3]) + row[6].replace(' ', '-')
+        video_title = str(row[6].replace(' ', '-'))
         video_path = config.MEDIA_ROOT + 'videos/' + str(row[5]) + '/' + str(row[3]) + '/'
         english_srt = video_path + video_title + '-English.srt'
         if os.path.isfile(english_srt):
