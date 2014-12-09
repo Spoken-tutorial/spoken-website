614cb88d0b0e76f533c60d36eac785657ed7a87f
vishnukraj <vishnukraj007@gmail.com>
2014-12-09 13:57:34 +0530

    remove video from youtube - v3 implementation

---
+++ youtube/core.py
@@ -0,0 +1,59 @@
+import os
+import httplib2
+
+from oauth2client import xsrfutil
+from apiclient.discovery import build
+from apiclient.http import MediaFileUpload
+from oauth2client.django_orm import Storage
+from oauth2client.client import flow_from_clientsecrets
+from django.conf import settings
+
+from youtube.models import *
+
+scope_urls = {
+    'readonly': 'https://www.googleapis.com/auth/youtube.readonly',
+    'manage': 'https://www.googleapis.com/auth/youtube',
+    'upload': 'https://www.googleapis.com/auth/youtube.upload',
+}
+
+scope_users = {
+    'readonly': User.objects.get(username = 'vishnukraj'),
+    'manage': User.objects.get(username = 'sanmugam'),
+    'upload': User.objects.get(username = 'nancy'),
+}
+
+scope_secret = settings.BASE_DIR + '/' + settings.YOUTUBE_SECRET_FILE
+
+def get_storage_flow_secret(scope):
+    flow = flow_from_clientsecrets(
+        scope_secret,
+        scope = scope_urls[scope],
+        redirect_uri = settings.YOUTUBE_REDIRECT_URL + scope,
+    )
+    storage = Storage(CredentialsModel, 'id', scope_users[scope], 'credential')
+    credential = storage.get()
+    return flow, credential
+
+def delete_video(service, video_id):
+    try:
+        result = service.videos().delete(id = video_id).execute()
+        return result
+    except Exception, e:
+        print e
+    return None
+
+def delete_playlist(service, playlist_id):
+    try:
+        result = service.playlists().delete(id = playlist_id).execute()
+        return playlist_delete_response
+    except Exception, e:
+        print e
+    return None
+
+def delete_playlistitem(service, playlist_item_id):
+    try:
+        result = service.playlistItems().delete(id = playlist_item_id).execute()
+        return result
+    except Exception, e:
+        print e
+    return None
