8f330e02e80d5d3a732390f613535d0b3a48a5fe
Sanmugasundaram K <k.sanmugam2@gmail.com>
2015-03-25 10:44:45 +0530

    foss field in learners

--- statistics/models.py
+++ statistics/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 # Create your models here.
-
+from creation.models import FossCategory
 class Learner(models.Model):
     fname = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
+    foss = models.ForeignKey(FossCategory, null=True)
     roll_no = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add = True, null=True)
     updated = models.DateTimeField(auto_now = True, null=True)
