17b181054f5484ef7c3290d6d822eae9b3ccbe30
Spoken-8 <kirti3192@gmail.com>
2016-01-13 15:48:21 +0530

    partial complete team page

--- team/urls.py
+++ team/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     TeamContributorListView.as_view(template_name="team_members.html"),
     name="team-contributor"
   ),
+  url(
+    r'^domain-reviewer/$',
+    TeamDomainListView.as_view(template_name="team_members.html"),
+    name="team-Domain"
+  ),
 ]
--- team/views.py
+++ team/views.py
@@ -8,8 +8,16 @@ from django.contrib.auth.models import Group, User
 
 class TeamContributorListView(ListView):
   queryset = None
-  paginate_by = 1000
+  paginate_by = 100
  
   def dispatch(self, *args, **kwargs):
     self.queryset = User.objects.filter(groups__name='Contributor')
     return super(TeamContributorListView, self).dispatch(*args, **kwargs)
+    
+class TeamDomainListView(ListView):
+  queryset = None
+  paginate_by = 100
+ 
+  def dispatch(self, *args, **kwargs):
+    self.queryset = User.objects.filter(groups__name='Domain-Reviewer')
+    return super(TeamDomainListView, self).dispatch(*args, **kwargs)
