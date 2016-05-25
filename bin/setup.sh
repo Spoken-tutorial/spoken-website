5ead963a131df230bc69589aaeafe09b0b7c97cd
Saurabh Kumar <saurabh@saurabh-kumar.com>
2016-05-25 11:57:11 +0530

    Add sample config.py and a bin/setup.sh

---
+++ bin/setup.sh
@@ -0,0 +1,2 @@
+echo "create database test_spoken;" | mysql -uroot
+mysql -uroot test_spoken < /path/to/test_spoken.sql
---
+++ config.sample.py
@@ -0,0 +1,43 @@
+DB = 'test_spoken'
+DB_USER = 'root'
+DB_PASS = ''
+MDB = 'test_spoken'
+MDB_USER = 'root'
+MDB_PASS = ''
+FDB = 'test_spoken'
+FDB_USER = 'root'
+FDB_PASS = ''
+CDB = 'test_spoken'
+CDB_USER = 'root'
+CDB_PASS = ''
+WDB = 'test_spoken'
+WDB_USER = ''
+WDB_PASS = ''
+
+ONLINE_TEST_URL = "http://10.177.6.252/moodle/"
+STVIDEOS_PATH = "/websites_dir/st_drupal/sites/default/files/"
+PROFILE_PATH = "/websites_dir/st_drupal/"
+DEBUG_MODE = True
+SCRIPT_URL = "http://script.spoken-tutorial.org/index.php/"
+DEBUG_MODE = True
+TZ_STATUS = True
+HTML_MINIFY = False
+
+# ANALYTICS_DATA = ''
+ANALYTICS_DATA = """
+"""
+ADMINISTRATOR_EMAIL = ()
+
+YOUTUBE_REDIRECT_URL = 'http://spoken-tutorial.org/youtube/oauth2callback/'
+YOUTUBE_SECRET_FILE = 'youtube/secrets/sptut_client_secret.json'
+YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+# events2
+ACADEMIC_DURATION = 4
+INDEX_PATH = ""
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
