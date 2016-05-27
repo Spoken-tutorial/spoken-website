eeea8d7a576389510cc68f0f366621b032a5d2a2
Saurabh Kumar <saurabh@saurabh-kumar.com>
2016-05-27 11:54:25 +0530

    Use pypi release of django-nicedit, compatible with django 1.8

--- bin/devserver.sh
+++ bin/devserver.sh
@@ -1,7 +1,4 @@
 pip install -r requirements-dev.txt
 
-# South is dependeny of django-nicedit, incompatible with django-1.8 remove it.
-pip uninstall -y south
-
 export DJANGO_SETTINGS_MODULE='spoken.settings_dev'
 python manage.py runserver
