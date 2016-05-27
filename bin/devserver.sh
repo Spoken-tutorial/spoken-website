42ff1bcd2260920758cc197136cbc302ba4ff5ff
Saurabh Kumar <saurabh@saurabh-kumar.com>
2016-05-27 12:34:02 +0530

    Add script to copy config.sample.py if not already exist

--- bin/devserver.sh
+++ bin/devserver.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
 pip install -r requirements-dev.txt
+
+if [ ! -f config.py ]; then
+    cp config.sample.py config.py
+fi
 
 export DJANGO_SETTINGS_MODULE='spoken.settings_dev'
 python manage.py runserver
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 pip install -r requirements-dev.txt
 
+if [ ! -f config.py ]; then
+    cp config.sample.py config.py
+fi
+
 echo "create database test_spoken;" | mysql -uroot
 mysql -uroot test_spoken < /path/to/test_spoken.sql
