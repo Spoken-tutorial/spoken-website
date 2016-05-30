b7cf851a5cee7cd6d8225be8e02656bf24e23b75
Saurabh Kumar <saurabh@saurabh-kumar.com>
2016-05-30 14:17:51 +0530

    fix whitespaces in events/urlsv2.py

--- events/urlsv2.py
+++ events/urlsv2.py
@@ -6,132 +6,132 @@ from events.formsv2 import *
 from events.urls import *
 urlpatterns = [
     url(
-      r'^training-planner/', 
-      TrainingPlannerListView.as_view(template_name="training_planner.html"), 
+      r'^training-planner/',
+      TrainingPlannerListView.as_view(template_name="training_planner.html"),
       name="training_planner"
     ),
     url(
-      r'^select-participants/', 
-      TrainingPlannerListView.as_view(template_name="select_participants.html"), 
+      r'^select-participants/',
+      TrainingPlannerListView.as_view(template_name="select_participants.html"),
       name="select_participants"
     ),
     url(
-      r'^student-batch/new/$', 
+      r'^student-batch/new/$',
       StudentBatchCreateView.as_view(template_name="new_batch.html", \
-        form_class=StudentBatchForm), 
+        form_class=StudentBatchForm),
       name="add_batch"
     ),
     url(
-      r'^student-batch/(?P<bid>\d+)/new/$', 
+      r'^student-batch/(?P<bid>\d+)/new/$',
       StudentBatchCreateView.as_view(template_name="update_batch.html", \
-        form_class=NewStudentBatchForm), 
+        form_class=NewStudentBatchForm),
       name="add_student"
     ),
     url(
-      r'^student-batch/(?P<pk>\d+)/$', 
+      r'^student-batch/(?P<pk>\d+)/$',
       StudentBatchUpdateView.as_view(template_name="edit_batch.html", \
-        form_class=UpdateStudentBatchForm), 
+        form_class=UpdateStudentBatchForm),
       name="edit_student"
     ),
     url(
-      r'^student-batch/edit/(?P<pk>\d+)/$', 
+      r'^student-batch/edit/(?P<pk>\d+)/$',
       StudentBatchYearUpdateView.as_view(template_name="edit_batch_year.html", \
-        form_class=UpdateStudentYearBatchForm), 
+        form_class=UpdateStudentYearBatchForm),
       name="edit_year"
     ),
     url(
-      r'^student-batch/(?P<bid>\d+)/view/$', 
-      StudentListView.as_view(template_name="list_student.html"), 
+      r'^student-batch/(?P<bid>\d+)/view/$',
+      StudentListView.as_view(template_name="list_student.html"),
       name="list_student"
     ),
     url(
-      r'^student-batch/$', 
-      StudentBatchListView.as_view(template_name="student_batch_list.html"), 
+      r'^student-batch/$',
+      StudentBatchListView.as_view(template_name="student_batch_list.html"),
       name="batch_list"
     ),
     url(
-      r'^(?P<tpid>\d+)/training-request', 
-      TrainingRequestCreateView.as_view(template_name="training_request.html"), 
+      r'^(?P<tpid>\d+)/training-request',
+      TrainingRequestCreateView.as_view(template_name="training_request.html"),
       name="training_request"
     ),
     url(
-      r'^(?P<tid>\d+)/attendance', 
+      r'^(?P<tid>\d+)/attendance',
       TrainingAttendanceListView.as_view(template_name=\
-        "training_attendance.html"), 
+        "training_attendance.html"),
       name="training_attendance"
     ),
     url(
-      r'^(?P<tid>\d+)/certificate', 
+      r'^(?P<tid>\d+)/certificate',
       TrainingCertificateListView.as_view(template_name=\
-        "training_certificate.html"), 
+        "training_certificate.html"),
       name="training_certificate"
     ),
     url(
-      r'^training-request/(?P<trid>\d+)/$', 
+      r'^training-request/(?P<trid>\d+)/$',
       TrainingRequestEditView.as_view(template_name=\
-        "edit_training_request.html"), 
+        "edit_training_request.html"),
       name="edit_training_request"
     ),
     url(
-      r'^(?P<bid>\d+)/student-delete/(?P<pk>\d+)/$', 
+      r'^(?P<bid>\d+)/student-delete/(?P<pk>\d+)/$',
       StudentDeleteView.as_view(template_name="student_delete.html", \
-        success_url="/software-training/student-batch"), 
+        success_url="/software-training/student-batch"),
       name="student_delete"
     ),
     url(
-      r'^training-certificate/(?P<taid>\d+)/organiser/$', 
+      r'^training-certificate/(?P<taid>\d+)/organiser/$',
       OrganiserTrainingCertificateView.as_view(), \
         name="organiser_training_certificate"
     ),
     url(
-      r'^training-certificate/(?P<taid>\d+)/student/$', 
+      r'^training-certificate/(?P<taid>\d+)/student/$',
       StudentTrainingCertificateView.as_view(), \
         name="student_training_certificate"
     ),
     url(
-      r'^course-map-list/$', 
-      CourseMapListView.as_view(template_name="coursemap_list.html"), 
+      r'^course-map-list/$',
+      CourseMapListView.as_view(template_name="coursemap_list.html"),
       name="coursemaplist"
     ),
     url(
-      r'^course-map/(?P<pk>\d+)$', 
+      r'^course-map/(?P<pk>\d+)$',
       CourseMapUpdateView.as_view(template_name=\
-        "coursemap.html"), 
+        "coursemap.html"),
       name="coursemapupdate"
     ),
 #    url(
-#      r'^single-training/pending/$', 
-#     SingleTrainingNewListView.as_view(template_name="single-training.html"), 
+#      r'^single-training/pending/$',
+#     SingleTrainingNewListView.as_view(template_name="single-training.html"),
 #      name="single-training-pending"
 #    ),
 #    url(
-#      r'^single-training/approved/$', 
-#     SingletrainingApprovedListView.as_view(template_name="single-training.html"), 
+#      r'^single-training/approved/$',
+#     SingletrainingApprovedListView.as_view(template_name="single-training.html"),
 #      name="single-training-approved"
 #    ),
 #    url(
-#      r'^single-training/rejected/$', 
-#     SingletrainingRejectedListView.as_view(template_name="single-training.html"), 
+#      r'^single-training/rejected/$',
+#     SingletrainingRejectedListView.as_view(template_name="single-training.html"),
 #      name="single-training-rejected"
 #    ),
 #    url(
-#      r'^single-training/ongoing/$', 
-#     SingletrainingOngoingListView.as_view(template_name="single-training.html"), 
+#      r'^single-training/ongoing/$',
+#     SingletrainingOngoingListView.as_view(template_name="single-training.html"),
 #      name="single-training-ongoing"
 #    ),
 #    url(
-#      r'^single-training/pendingattendance/$', 
-#     SingletrainingPendingAttendanceListView.as_view(template_name="single-training.html"), 
+#      r'^single-training/pendingattendance/$',
+#     SingletrainingPendingAttendanceListView.as_view(template_name="single-training.html"),
 #      name="single-training-pendingattendance"
 #    ),
 #    url(
-#      r'^single-training/completed/$', 
-#     SingletrainingCompletedListView.as_view(template_name="single-training.html"), 
+#      r'^single-training/completed/$',
+#     SingletrainingCompletedListView.as_view(template_name="single-training.html"),
 #      name="single-training-completed"
 #    ),
     url(
-      r'^single-training/new/$', 
-     SingletrainingCreateView.as_view(template_name="single-training-form.html"), 
+      r'^single-training/new/$',
+     SingletrainingCreateView.as_view(template_name="single-training-form.html"),
       name="new-single-training"
     ),
     url(
@@ -140,8 +140,8 @@ urlpatterns = [
       name="update-single-training"
     ),
     url(
-      r'^single-training/(?P<status>\w+)/$', 
-     SingleTrainingListView.as_view(template_name="single-training.html"), 
+      r'^single-training/(?P<status>\w+)/$',
+     SingleTrainingListView.as_view(template_name="single-training.html"),
       name="single-training-list"
     ),
     url(
@@ -150,78 +150,78 @@ urlpatterns = [
       name="markcomplete-single-training"
     ),
     url(
-      r'^single-training/(?P<tid>\d+)/certificate', 
+      r'^single-training/(?P<tid>\d+)/certificate',
       SingleTrainingCertificateListView.as_view(template_name=\
-        "single-training-certificate.html"), 
+        "single-training-certificate.html"),
       name="single-training-certificate"
     ),
     url(
-      r'^single-training-certificate/(?P<taid>\d+)/organiser/$', 
+      r'^single-training-certificate/(?P<taid>\d+)/organiser/$',
       OrganiserSingleTrainingCertificateView.as_view(), \
         name="organiser_singletraining_certificate"
     ),
     #ajax
     url(
-      r'^save-student/', 
+      r'^save-student/',
       SaveStudentView.as_view()
     ),
     url(
-      r'^get-course-option/', 
+      r'^get-course-option/',
       GetCourseOptionView.as_view()
     ),
     url(
-      r'^get-batch-option/', 
+      r'^get-batch-option/',
       GetBatchOptionView.as_view()
     ),
     url(
-      r'^get-batch-course-status/', 
+      r'^get-batch-course-status/',
       GetBatchStatusView.as_view()
     ),
     url(
-      r'^training-request/(?P<role>\w+)/(?P<status>\w+)/$', 
-      TrainingRequestListView.as_view(template_name='training_list.html'), 
+      r'^training-request/(?P<role>\w+)/(?P<status>\w+)/$',
+      TrainingRequestListView.as_view(template_name='training_list.html'),
       name='training_list'
     ),
     #url(r'^get-language-option/', GetLanguageOptionView.as_view()),
     url(
       r'^single-training/pending/(?P<pk>\d+)/approve/$',
-      'events.viewsv2.SingleTrainingApprove', 
+      'events.viewsv2.SingleTrainingApprove',
       name="single-training-approve"
     ),
     url(
-      r'^single-training/pending/(?P<pk>\d+)/reject/$', 
-      'events.viewsv2.SingleTrainingReject', 
+      r'^single-training/pending/(?P<pk>\d+)/reject/$',
+      'events.viewsv2.SingleTrainingReject',
       name="single-training-reject"
     ),
     url(
-      r'^single-training/pending/(?P<pk>\d+)/requestmarkattendance/$', 
-      'events.viewsv2.SingleTrainingPendingAttendance', 
+      r'^single-training/pending/(?P<pk>\d+)/requestmarkattendance/$',
+      'events.viewsv2.SingleTrainingPendingAttendance',
       name="single_training_pending"
     ),
     url(
-      r'^markas/(?P<pk>\d+)/complete/$', 
-      'events.viewsv2.MarkAsComplete', 
+      r'^markas/(?P<pk>\d+)/complete/$',
+      'events.viewsv2.MarkAsComplete',
       name="mark_as_complete"
     ),
     url(
-      r'^mark/(?P<pk>\d+)/complete/$', 
-      'events.viewsv2.MarkComplete', 
+      r'^mark/(?P<pk>\d+)/complete/$',
+      'events.viewsv2.MarkComplete',
       name="mark_complete"
     ),
     url(
-      r'^single-training/(?P<tid>\d+)/attendance', 
+      r'^single-training/(?P<tid>\d+)/attendance',
       SingleTrainingAttendanceListView.as_view(template_name=\
-        "single-training-attendance.html"), 
+        "single-training-attendance.html"),
       name="single_training_attendance"
      ),
      url(
-      r'^organiser-feedback/', 
-      OrganiserFeedbackCreateView.as_view(template_name='organiser_feedback.html'), 
+      r'^organiser-feedback/',
+      OrganiserFeedbackCreateView.as_view(template_name='organiser_feedback.html'),
       name='organiser_feedback'
     ),
     url(
-      r'^organiser-feedback-display/$', 
-     OrganiserFeedbackListView.as_view(template_name="organiser_feedback_display.html"), 
+      r'^organiser-feedback-display/$',
+     OrganiserFeedbackListView.as_view(template_name="organiser_feedback_display.html"),
       name="organiser-feedback-display"
     ),
     url(
@@ -232,12 +232,12 @@ urlpatterns = [
     ),
     url(
       r'^old-training/(?P<tid>\d+)/participant/$',
-      OldStudentListView.as_view(template_name="old_list_student.html"), 
+      OldStudentListView.as_view(template_name="old_list_student.html"),
       name="old_list_student"
     ),
     url(
       r'^old-training/(?P<tid>\d+)/close/$',
-      OldTrainingCloseView.as_view(template_name=""), 
+      OldTrainingCloseView.as_view(template_name=""),
       name="old_training_close"
     ),
     url(
