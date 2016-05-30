43eb6d77d41aaac7b536af725874593f6164e6f2
Saurabh Kumar <saurabh@saurabh-kumar.com>
2016-05-30 10:04:48 +0530

    Make setup.sh and devserver.sh more human friendly

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-pip install -r requirements-dev.txt
+set -e
+
+echo ">> Install python requirements from 'requirements-dev.txt'"
+pip install -r requirements-dev.txt > /dev/null
 
 if [ ! -f config.py ]; then
+    echo ">> Copying 'config.sample.py -> config.py'"
     cp config.sample.py config.py
 fi
 
+echo ">> Provide your local mysql credentials in order to import inital test database."
+read -p "MySQL Username:" db_user
+read -sp "MySQL Password:" db_pass
+read -p "MySQL sqldump path:" db_dump
+
 echo "create database test_spoken;" | mysql -uroot
-mysql -uroot test_spoken < /path/to/test_spoken.sql
+mysql --user=$db_user --password=$db_pass test_spoken < $db_dump
