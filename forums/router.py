6325837f30dede9467d1270f706439fd56f942a1
Saurabh Kumar <saurabh@saurabh-kumar.com>
2016-06-10 06:43:02 +0530

    style(forums): make it PEP8 complaint, remove unsed imports and print statements

--- forums/router.py
+++ forums/router.py
@@ -1,23 +1,21 @@
 class ForumsRouter(object):
-	"""
-		A router to manage database operations in the forums app.
-	"""
-	def db_for_read(self, model, **hints):
-		"""
-			Point all read operations on forums app to spoken
-			database.
-		"""
-		if model._meta.app_label == 'forums':
-			return 'forums'
-		return None
-	
-	def db_for_write(self, model, **hints):
-		if model._meta.app_label == 'forums':
-			return 'forums'
-		return 'default'
+    """A router to manage database operations in the forums app.
+    """
 
-	def allow_relation(self, obj1, obj2, **hints):
-		return True
+    def db_for_read(self, model, **hints):
+        """Point all read operations on forums app to spoken database.
+        """
+        if model._meta.app_label == 'forums':
+            return 'forums'
+        return None
 
-	def allow_migrate(self, db, model):
-		return True
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label == 'forums':
+            return 'forums'
+        return 'default'
+
+    def allow_relation(self, obj1, obj2, **hints):
+        return True
+
+    def allow_migrate(self, db, model):
+        return True
