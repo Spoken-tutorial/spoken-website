fa93b3f142714a0a86fd29f85678ae2c4acabdae
Saurabh Kumar <saurabh@saurabh-kumar.com>
2016-06-10 06:49:00 +0530

    style(cdeep): make it PEP8 complaint, remove unsed imports and print statements

--- cdeep/router.py
+++ cdeep/router.py
@@ -1,23 +1,21 @@
 class CdeepRouter(object):
-	"""
-		A router to manage database operations in the cdeep app.
-	"""
-	def db_for_read(self, model, **hints):
-		"""
-			Point all read operations on cdeep app to spoken
-			database.
-		"""
-		if model._meta.app_label == 'cdeep':
-			return 'cdeep'
-		return None
-	
-	def db_for_write(self, model, **hints):
-		if model._meta.app_label == 'cdeep':
-			return 'cdeep'
-		return 'default'
+    """A router to manage database operations in the cdeep app.
+    """
 
-	def allow_relation(self, obj1, obj2, **hints):
-		return True
+    def db_for_read(self, model, **hints):
+        """Point all read operations on cdeep app to spoken database.
+        """
+        if model._meta.app_label == 'cdeep':
+            return 'cdeep'
+        return None
 
-	def allow_migrate(self, db, model):
-		return True
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label == 'cdeep':
+            return 'cdeep'
+        return 'default'
+
+    def allow_relation(self, obj1, obj2, **hints):
+        return True
+
+    def allow_migrate(self, db, model):
+        return True
