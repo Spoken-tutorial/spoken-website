d71223bb45da9af3a4078df090c4bea8c48cc95b
Vishnuraj Kamalanathan <vishnu@simplotel.com>
2016-07-18 21:45:09 +0530

    created default migration files for all apps

---
+++ statistics/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('creation', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Learner',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('fname', models.CharField(max_length=50)),
+                ('email', models.EmailField(max_length=50)),
+                ('roll_no', models.CharField(max_length=50)),
+                ('created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('updated', models.DateTimeField(auto_now=True, null=True)),
+                ('foss', models.ForeignKey(to='creation.FossCategory', null=True)),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='learner',
+            unique_together=set([('email',)]),
+        ),
+    ]
