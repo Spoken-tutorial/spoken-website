8d6cd57326055dcb101a0a8c353047e1adcaaca4
Sanmugasundaram K <k.sanmugam2@gmail.com>
2017-04-05 10:46:46 +0530

    [Fix] django.db.migrations.graph.NodeNotFoundError: Migration certificate.0006_merge dependencies reference nonexistent parent node (u'certificate', u'0005_auto_20160827_2323')

--- certificate/migrations/0006_merge.py
+++ certificate/migrations/0006_merge.py
@@ -7,7 +7,6 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('certificate', '0005_auto_20160827_2323'),
         ('certificate', '0005_auto_20170107_1845'),
     ]
 
