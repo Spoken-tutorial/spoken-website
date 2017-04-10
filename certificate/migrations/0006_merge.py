d5d39dfe25237985a2bf158272530736590f3701
shailesh <shailesh@spoken>
2017-04-10 17:25:43 +0530

    Revert "Revert "commiting certificate merge migration file""
    This reverts commit 516734fd8672d2f016cf24075013608a67109c58.

---
+++ certificate/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('certificate', '0005_auto_20160827_2323'),
+        ('certificate', '0005_auto_20170107_1845'),
+    ]
+
+    operations = [
+    ]
