3707d8dd2cd251d1250be7ff37bebdae777333ea
shailesh <shailesh@spoken>
2017-06-06 17:00:08 +0530

    test and no-test categorization of FOSSes

--- spoken/forms.py
+++ spoken/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.db.models import Count, Q
 
 # Spoken Tutorial Stuff
-from creation.models import TutorialResource
+from creation.models import TutorialResource, FossAvailableForTest
 from events.models import Testimonials
 
 
@@ -13,11 +13,34 @@ class KeywordSearchForm(forms.Form):
 
 class TutorialSearchForm(forms.Form):
     try:
+        all_tests = FossAvailableForTest.objects.filter(status=1)
+        test_list = []
+        for item in all_tests:
+            test_list.append(item.foss.foss)
+
         foss_list = TutorialResource.objects.filter(Q(status=1) | Q(status=2), language__name='English').values('tutorial_detail__foss__foss').annotate(
             Count('id')).order_by('tutorial_detail__foss__foss').values_list('tutorial_detail__foss__foss', 'id__count').distinct()
         choices = [('', '-- All Courses --'), ]
+
+        # segregate list for foss courses with and without tests
+        choices.append(['', ''])
+        choices.append(['', '-------------------------------------------------------------------'])
+        choices.append(['', '        FOSS Courses available for tests'])
+        choices.append(['', '-------------------------------------------------------------------'])
+        choices.append(['', ''])
         for foss_row in foss_list:
-            choices.append((str(foss_row[0]), str(foss_row[0]) + ' (' + str(foss_row[1]) + ')'))
+            if foss_row[0] in test_list:
+                choices.append((str(foss_row[0]), str(foss_row[0]) + ' (' + str(foss_row[1]) + ')'))
+
+        choices.append(['', ''])
+        choices.append(['', '-------------------------------------------------------------------'])
+        choices.append(['', '        FOSS Courses NOT available for tests'])
+        choices.append(['', '-------------------------------------------------------------------'])
+        choices.append(['', ''])
+        for foss_row in foss_list:
+            if foss_row[0] not in test_list:
+                choices.append((str(foss_row[0]), str(foss_row[0]) + ' (' + str(foss_row[1]) + ')'))
+
         search_foss = forms.ChoiceField(
             choices=choices,
             widget=forms.Select(),
