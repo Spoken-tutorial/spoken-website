4907227aa258a99ec564265f34360d1639c8e850
ShaileshDivey <shailesh.j.divey@gmail.com>
2017-06-20 10:46:04 +0530

    Categorize foss by test (#160)
    * test and no-test categorization of FOSSes
    
    * no change to test/no-test page yet
    
    * modified to make the test/no-test categorizations non-clickable

--- spoken/forms.py
+++ spoken/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.db.models import Count, Q
 
 # Spoken Tutorial Stuff
-from creation.models import TutorialResource
+from creation.models import TutorialResource, FossAvailableForTest
 from events.models import Testimonials
 
 
@@ -13,13 +13,31 @@ class KeywordSearchForm(forms.Form):
 
 class TutorialSearchForm(forms.Form):
     try:
+        all_tests = FossAvailableForTest.objects.filter(status=1)
+        test_list = []
+        for item in all_tests:
+            test_list.append(item.foss.foss)
+
         foss_list = TutorialResource.objects.filter(Q(status=1) | Q(status=2), language__name='English').values('tutorial_detail__foss__foss').annotate(
             Count('id')).order_by('tutorial_detail__foss__foss').values_list('tutorial_detail__foss__foss', 'id__count').distinct()
-        choices = [('', '-- All Courses --'), ]
+
+        all_courses = [('', '-- All Courses --')]
+        test_choices = []
+        no_test_choices = []
+
         for foss_row in foss_list:
-            choices.append((str(foss_row[0]), str(foss_row[0]) + ' (' + str(foss_row[1]) + ')'))
+            if foss_row[0] in test_list:
+                test_choices.append((str(foss_row[0]), str(foss_row[0]) + ' (' + str(foss_row[1]) + ')'))
+
+        for foss_row in foss_list:
+            if foss_row[0] not in test_list:
+                no_test_choices.append((str(foss_row[0]), str(foss_row[0]) + ' (' + str(foss_row[1]) + ')'))
+
+        all_choices = (('', all_courses), ('', ''), ('FOSS Courses available for tests', tuple(test_choices)),
+                       ('', ''), ('FOSS Courses NOT available for tests', tuple(no_test_choices)))
+
         search_foss = forms.ChoiceField(
-            choices=choices,
+            choices=all_choices,
             widget=forms.Select(),
             required=False,
         )
