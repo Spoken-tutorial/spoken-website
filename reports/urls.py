9b58e7290123181ebb685826736d1442428e52da
Sanmugasundaram K <k.sanmugam2@gmail.com>
2017-06-21 23:40:31 +0530

    Option to export training statistics report data

--- reports/urls.py
+++ reports/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
+    url(r'events/training/csv/$', 'reports.views.events_training_csv', name='events_training_csv'),
     url(r'(?P<app_label>[\d\w]+)/(?P<model_name>[\d\w]+)/csv/$', 'reports.views.export_csv', name='export_csv'),
     url(r'(?P<app_label>[\d\w]+)/(?P<model_name>[\d\w]+)/report/$', 'reports.views.report_filter', name='report_filter'),
     url(r'elibrary/$', 'reports.views.elibrary', name='elibrary'),
