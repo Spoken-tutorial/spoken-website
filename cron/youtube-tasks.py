71b62ca7e78ece27f44bdc3b6435e472a59598c0
Vishnuraj Kamalanathan <vishnukraj007@gmail.com>
2017-08-03 12:35:42 +0530

    updated youtube tasks

--- cron/youtube-tasks.py
+++ cron/youtube-tasks.py
@@ -5,8 +5,7 @@ from django.db.models import Q
 
 # setting django environment
 from django.core.wsgi import get_wsgi_application
-# sys.path.append("/websites_dir/django_spoken/spoken")
-sys.path.append("/home/vishnu/mine/spoken-website")
+sys.path.append("/websites_dir/django_spoken/")
 os.environ["DJANGO_SETTINGS_MODULE"] = "spoken.settings"
 application = get_wsgi_application()
 
@@ -14,6 +13,6 @@ from config import *
 from youtube.tasks import upload_videos, create_playlists, append_to_playlist
 
 
-# upload_videos()
-# create_playlists()
+upload_videos()
+create_playlists()
 append_to_playlist()
