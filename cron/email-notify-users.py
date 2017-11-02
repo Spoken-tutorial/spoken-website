8932720ddcae1177834024f333027e58a42d4bc4
Spoken-8 <kirti3192@gmail.com>
2017-11-02 18:20:23 +0530

    induction:game reminder email

--- cron/email-notify-users.py
+++ cron/email-notify-users.py
@@ -14,63 +14,64 @@ from django.template import Context
 
 from config import *
 from cms.models import *
-users = User.objects.filter(is_active = 1)
+from events.models import InductionInterest
+
+#here fetch all user
+users = InductionInterest.objects.filter(
+    (Q(age ='20to25') | Q(age = '26to30') | Q(age = '31to35')),
+    (Q(experience_in_college = 'Lessthan1year') | Q(experience_in_college = 'Morethan1yearbutlessthan2years'))
+    ).exclude(designation = 'Other').values_list('email',flat=True).distinct()
 sent = 0
 notsent = 0
 count = 0
 tot_count = len(users)
 
-subject = '"HT for Mumbai" Award: Please vote for Prof. Kannan Moudgalya.'
+subject = '"Reminder to complete and submit your assignment "'
 
-success_log_file_head = open(LOG_ROOT+'happy-new-year-reminder-notify.txt',"w")
+success_log_file_head = open(LOG_ROOT+'induction_particiants_reminder.txt',"w")
 for user in users:
     message = '''
+Dear Participant,
 
-Greetings from the Spoken Tutorial Project, IIT Bombay.
-
-Prof Kannan Moudgalya, who leads the efforts of the Spoken Tutorial Project, has been nominated for the Hindustan Times "HT for Mumbai awards" 2015.
-
-From his profile that appeared in Hindustan Times (http://spoken-tutorial.org/media/news/772/772.pdf), you will see that this is a recognition of our IT literacy campaign through Spoken Tutorials.
-
-Pls visit this link and vote for him under the individual nominees category. His victory is the Project's victory.
-http://spoken-tutorial.org/ht-for-mumbai-nomination/
+This is to remind you that the last date of submission of assignment is 3 November 2017, 5 p.m.  You have to complete all 3 Knack games - Bomba Blitz, Meta Maze and Dashi Dash,  in order for your profile and score to be generated on our interface.  You also have to use the Knack code sent to you in your registered email id.
 
 
-Spread the word and help him and our Project win! 
+If you have already done so, pls ignore this mail.
 
-Last date for voting is 9 Jan 2016.
 
-Thank you in advance.
+But if you have not done so, pls complete the assignment before the deadline mentioned above.
 
---
 Regards,
-Spoken Tutorial Team,
-IIT Bombay.'''.format(user.username)
+Organising Team
+Induction Training Programme
+IIT Bombay
 
-    to  = [user.email]
-    #to = ['k.sanmugam2@gmail.com']
+'''.format(user)
+
+    to  = [user]
+    # to = ['ganeshmohite96@gmail.com','nancyvarkey@gmail.com']
     email = EmailMultiAlternatives(
-        subject, message, 'administrator@spoken-tutorial.org',
+        subject, message, 'workshops@spoken-tutorial.org',
         to = to,
         headers = {
          "Content-type" : "text/html"
         }
     )
     #email.attach_alternative(html_content, "text/html")
+    # email.attach_file('/websites_dir/django_spoken/spoken/cron/Knack-user-flow.pdf')
     count = count + 1
     try:
         result = email.send(fail_silently=False)
         sent += 1
-        # cur.execute("INSERT INTO organiser_sent_email (user_id) VALUES (" + str(user.id) + ")")
         if sent%100 == 0:
             time.sleep(10)
         #print to," => sent (", str(count),"/",str(tot_count),")"
-        success_log_file_head.write(str(user.id)+','+str(1)+'\n')
+        success_log_file_head.write(str(user)+','+str(1)+'\n')
     except Exception, e:
         print e
         #print to," => not sent (",count,"/",tot_count,")"
-        success_log_file_head.write(str(user.id)+','+str(0)+'\n')
-    #break
+        success_log_file_head.write(str(user)+','+str(0)+'\n')
+    # break
 print "--------------------------------"
 print "Total sent mails:", sent
 print "Total not sent mails:", notsent
