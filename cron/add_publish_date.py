438e1588c42df182a6567ac6e6a8dd62a252e624
kirti <kirti3192@gmail.com>
2018-01-17 15:36:50 +0530

    writing success log in txt file and maintaining count

--- cron/add_publish_date.py
+++ cron/add_publish_date.py
@@ -12,12 +12,21 @@ application = get_wsgi_application()
 from config import *
 from creation.models import TutorialResource, PublishTutorialLog
 
-tutorials = TutorialResource.objects.all()
-for tutorial in tutorials:
-	latest_publish_date = PublishTutorialLog.objects.filter(tutorial_resource_id = tutorial.id).order_by('-id').first()
-	if latest_publish_date:
-		tutorial.publish_date = latest_publish_date.created
-	else :
-		tutorial.publish_date = tutorial.created
-	tutorial.save()
+with open(LOG_ROOT+'publish_date_log.txt',"w") as success_log_file_head:
+	count =0 
 
+	tutorials = TutorialResource.objects.all()
+	for tutorial in tutorials:
+		count = count + 1
+		try:
+			latest_publish_date = PublishTutorialLog.objects.filter(tutorial_resource_id = tutorial.id).order_by('-id').first()
+			if latest_publish_date:
+				tutorial.publish_date = latest_publish_date.created
+			else :
+				tutorial.publish_date = tutorial.created
+			tutorial.save()		
+			success_log_file_head.write(str(tutorial.id)+','+str(1)+'\n')
+		except Exception, e:
+			print e
+			success_log_file_head.write(str(tutorial.id)+','+str(0)+'\n')
+	success_log_file_head.write('------- tutorials publish date updated ---------- :'+str(count))
