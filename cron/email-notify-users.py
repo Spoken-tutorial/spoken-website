8973835e019c2dcab5dcc57a734dd9047ecad6cb
Sanmugasundaram K <k.sanmugam2@gmail.com>
2018-04-19 17:35:32 +0530

    live changes in email script

--- cron/email-notify-users.py
+++ cron/email-notify-users.py
@@ -12,7 +12,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
 
-from config import 
+from config import * 
 from cms.models import *
 from events.models import Drupal2018_email
 
@@ -25,12 +25,12 @@ tot_count = len(users)
 
 subject = ' Invitation to attend "Drupal in a Day" workshop'
 
-success_log_file_head = open(LOG_ROOT+'induction_particiants_thirdshortlist.txt',"w")
+success_log_file_head = open(LOG_ROOT+'drupal2018_org_invitation.txt',"w")
 for user in users:
     message = '''
 <p>Dear Future Professionals,</p>
 
-<p>Spoken Tutorial in association with FOSSEE, IIT Bombay, is hosting "Drupal-in-a-Day" workshop alongside Drupal Camp Mumbai 2018 at IIT Bombay. We invite you to attend this workshop and learn Drupal, the world’s leading open source content management framework, for developing websites and web based applications (for desktop and mobile). On successful completion of the surprise activity, you also stand a good chance to get a free participation ticket to attend two days of Drupal Camp on 28 and 29 April 2018.</p>
+<p>Spoken Tutorial in association with FOSSEE, IIT Bombay, is hosting "Drupal-in-a-Day" workshop alongside Drupal Camp Mumbai 2018 at IIT Bombay. We invite you to attend this workshop and learn Drupal, the world\'s leading open source content management framework, for developing websites and web based applications (for desktop and mobile). On successful completion of the surprise activity, you also stand a good chance to get a free participation ticket to attend two days of Drupal Camp on 28 and 29 April 2018.</p>
 
 <p>Drupal Camp Mumbai is one of the largest Drupal community gatherings in India. The purpose of this event is to bring students, developers, architects, managers, businesses and organisations on a singular platform to network, interact and share openly with each other, and help strengthen the community at large.</p>
 
@@ -44,17 +44,17 @@ for user in users:
 
 <br><b>To know more and Register:-</b> <a href="http://spoken-tutorial.org/workshop/drupal2018/" target="_blank">Click Here</a></p>
 
-<p>Thanks and rgds,
-Organising Team
-Spoken Tutorial, FOSSEE and DCM
+<p>Thanks and rgds,<br>
+Tejas Shah, Organising Team<br>
+Spoken Tutorial, FOSSEE and DCM<br>
 IIT Bombay</p>
 
 '''.format(user.email)
 
     to  = [user.email]
-    # to = ['ganeshmohite96@gmail.com','nancyvarkey@gmail.com']
+    #to = ['ganeshmohite96@gmail.com','nancyvarkey.iitb@gmail.com']
     email = EmailMultiAlternatives(
-        subject, message, 'workshops@spoken-tutorial.org',
+        subject, message, 'tejas.shah@iitb.ac.in',
         to = to,
         headers = {
          "Content-type" : "text/html"
@@ -74,7 +74,7 @@ IIT Bombay</p>
         print e
         #print to," => not sent (",count,"/",tot_count,")"
         success_log_file_head.write(str(user.email)+','+str(0)+'\n')
-    # break
+    #break
 print "--------------------------------"
 print tot_count
 print "Total sent mails:", sent
