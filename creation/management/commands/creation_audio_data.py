f5df1cb7b3fd049ea31ef6a348f73ec894e288d4
Ajay Tripathi <ajay39in@gmail.com>
2018-06-12 14:56:25 +0530

    Requirements updated + Minor Fixes

--- creation/management/commands/creation_audio_data.py
+++ creation/management/commands/creation_audio_data.py
@@ -1,7 +1,7 @@
 # Code to add publish date in tutorialresource table
 # To run this file please follow below instructions:
 # 1. Go to project directory
-# 2. run "python manage.py add_publish_date"
+# 2. run "python manage.py creation_audio_data"
 
 from __future__ import absolute_import, print_function, unicode_literals
 
@@ -17,13 +17,12 @@ class Command(BaseCommand):
     @tx.atomic
     def handle(self, *args, **options):
         '''
-        Script to add field `audio` in `creation_tutorialresource` 
-        and change path of the video saved in accordance to 
-        the new file system.
+        Script to change path of the video saved in accordance to 
+        the new file system & populate audio path column for existing rows.
         '''
         # Get data from database
         try:
-            # Check if the `audio` filed exists in the database.
+            # Check if the `audio` field exists in the database.
             field_exist_check = TutorialResource._meta.get_field('audio')
             existing_video_list = TutorialResource.objects.filter(video__gt="").values_list("pk", "video", "language_id__name")
             for row in existing_video_list:
@@ -32,6 +31,6 @@ class Command(BaseCommand):
                 if audio_name.rsplit('-',1)[1] != "Video":
                     TutorialResource.objects.filter(pk=row[0]).update(audio=audio_name+".ogg", video=audio_name.rsplit(row[2])[0]+"Video.webm")
         except FieldDoesNotExist:
-            return False
+            print ("1001: No changes have occured, check if migration is done properly", error)
         except Exception as error:
-            print ("1001:", error)
+            print ("1002:", error)
