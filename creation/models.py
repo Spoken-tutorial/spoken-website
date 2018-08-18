01097a0b6a72d5571588b2105beefb04d1a90e46
Sammy <you@example.com>
2018-08-18 10:26:46 +0530

    Role Requests

--- creation/models.py
+++ creation/models.py
@@ -338,14 +338,16 @@ class QualityReviewerNotification(models.Model):
 class RoleRequest(models.Model):
     user = models.ForeignKey(User, related_name='user')
     role_type = models.IntegerField(default=0)
+    language = models.ForeignKey(Language,null=True)
     status = models.PositiveSmallIntegerField(default=0)
+    language = models.ForeignKey(Language,null=True)
     approved_user = models.ForeignKey(
-        User, related_name='approved_user', null=True, blank=True)
+        User, related_name='approved_user', null=True, blank =True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        unique_together = (('user', 'role_type',),)
+        unique_together = (('user', 'role_type','language'),)
 
 
 class FossAvailableForWorkshop(models.Model):
