ba73e4c417fd6bcbef1098fb67b82ff860edf060
Kirti Ambre <kirti3192@gmail.com>
2018-09-28 16:14:59 +0530

    Testimo (#223)
    * Basic form created
    
    * Added Testimonial View page and completed upload pages
    
    * v0.1.0-beta - Media Testimonial
    
    * code documentation
    
    * Migrations Fixed
    
    * Test Cases for the testimonials module
    
    * [v0.2.0-beta] Media testimonials
    
    * [v0.3.0-beta] Media Testimonials
    
    * [Fix] Add media/testimonials folder to .gitignore
    
    * Client side check for duration of uploaded testimonial.
    
    * fixed migrations
    
    * testimonial main tab work
    
    * increased testimonial video size

--- creation/templatetags/creationdata.py
+++ creation/templatetags/creationdata.py
@@ -101,6 +101,17 @@ def get_missing_component_reply(mcid):
         replies = '<br /><b>Replies:</b>' + replies
     return replies
 
+
+
+def formatismp4(path):
+    '''
+    ** Registered to be used in jinja template **
+    Function takes in a file name and checks if the 
+    last 3 characters are `mp4`.
+    '''
+    return path[-3:] == 'mp4'
+
+
 def instruction_sheet(foss, lang):
     file_path = settings.MEDIA_ROOT + 'videos/' + str(foss.id) + '/' + foss.foss.replace(' ', '-') + '-Instruction-Sheet-' + lang.name + '.pdf'
     if lang.name != 'English':
@@ -232,6 +243,7 @@ def get_mp4_video(tr):
 register.inclusion_tag('spoken/templates/tutorial_search_form.html')(tutorialsearch)
 #register.filter('tutorialsearch', tutorialsearch)
 register.filter('get_timed_script', get_timed_script)
+register.filter('formatismp4', formatismp4)
 register.filter('get_prerequisite_from_td', get_prerequisite_from_td)
 register.filter('get_prerequisite', get_prerequisite)
 register.filter('get_video_visits', get_video_visits)
