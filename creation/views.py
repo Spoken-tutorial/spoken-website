0e2156a5fef50c31dc0a785714107f1c438a22d5
Sammy <you@example.com>
2018-11-06 02:52:35 +0530

    Username altered with Fname & Lname

--- creation/views.py
+++ creation/views.py
@@ -3216,7 +3216,7 @@ def payment_honorarium_download(request,hono_id):
     hr, min = divmod(totalSecs, 60)
     #print "%d:%02d:%02d" % (hr, min, sec),
 
-    user = payment_details.distinct().values('user__username','user__email')
+    user = payment_details.distinct().values('user__first_name','user__last_name')
 
     pdf_data ={}
     amount = payment_details.aggregate(Sum('amount'))
@@ -3235,7 +3235,7 @@ def payment_honorarium_download(request,hono_id):
     pdf_data['totalSecs'] = str(hr)+':'+str(min)+':'+str(sec)
     try:
         pdf_data['rate'] = rate[user_type[0]['user_type']]
-        file_name = str(user[0]['user__username'])+ " ST"
+        file_name = str(user[0]['user__first_name'])+ " ST"
     except IndexError:
         pdf_data['rate'] = 0
         file_name = "ST"
