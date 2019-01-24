e1d1e7a58555e70c51a8bf7830565f1c9657db46
kirti <kirti3192@gmail.com>
2019-01-24 16:25:58 +0530

    payment raminder mail

--- cron/email-notify-users.py
+++ cron/email-notify-users.py
@@ -14,49 +14,37 @@ from django.template import Context
 
 from config import * 
 from cms.models import *
-from events.models import Organiser
+from events.models import Organiser, AcademicCenter
 
 #here fetch all user
-users = Organiser.objects.filter(status=1)
+organisers = Organiser.objects.exclude(academic__institution_type_id__in=[3,2,15,5,13,4])
 sent = 0
 notsent = 0
 count = 0
-tot_count = len(users)
+tot_count = len(organisers)
 
-subject = ' Spoken Tutorial, IIT Bombay - Change in Training Policy'
+subject = ' *IIT Bombay - Spoken Tutorial Program | Invitation to colleges for 2019* '
 
-success_log_file_head = open(LOG_ROOT+'organiser_charge_list.txt',"w")
-for user in users:
+success_log_file_head = open(LOG_ROOT+'organiser_payment_reminder.txt',"w")
+for organiser in organisers:
     message = '''
-<p>
-Dear Faculty Organiser,
-</p>
+<p>Dear Faculty Organiser,<p>
 
-<p>
-We are truly happy to be associated with you as a Knowledge Partner for the past few years.  We provided Spoken Tutorial Software/ IT Courses training in your college and have grown together. The team at IIT Bombay has continued to offer new and exotic courses of world class quality, so essential for our students to get an employability worthy Skill Set. 
-</p>
+<p>Many thanks to you and your College for being a part of the IIT Bombay,Spoken Tutorial Software training program. You have contributed in a great way to its mega success. <p>
 
-<p>
-The new academic year will start soon - this is the right time to plan. You have already submitted planners & calendars in advance, covering the next 2 semesters. Be assured we will be with you all the way guiding, enabling & enhancing the quality of our delivery. In this connection, I wish to inform you of a policy change that has come to us from MHRD Govt. Of India. <u>Effective July, 2018 we are expected to charge a nominal annual amount from the colleges we are working with.</u> This decision has been driven by the fact that we are a mature and successful program, therefore from now on need to generate revenue and sustain our Project financially, on our own. The amount is <b><i>Rs. 25,000 per year</i></b> to be paid by colleges for an unlimited number of Training Workshops & Certification Tests taken by any number of their students & staff. There is no upper limit. Note also, we will not charge anything to colleges who have just come into the program and might train less than 100 students in a year.
-</p>
+<p>In August 2018 we informed you about the Training Policy change namely colleges will need to make a Payment/annual User fee of Rs.25000/- in order to continue the Spoken Tutorial Training. Many of you have immediately responded positively and made the payment. <u>To continue to benefit from the Software Training courses we offer - Payment is a must.  </u> </p>
 
-<p>
-We did extensive enquiries with our Engg., as well as Arts, Science, Commerce colleges and found out pleasantly that this amount was very manageable. In fact, considering a simple calculation for providing Basic Computer Skills - say getting our LibreOffice package training with Certificates of Word Processing, Spreadsheet & Slide presentation  at Rs.200/ student - just for 125 students the amount works out to a total of Rs. 25000/year/college. Most of you will be training 100s of students as you move to additional courses and more departments. You will roll out our Drupal, OpenFOAM, Java, C/C++, PHP, Linux, GIT and many other lucrative courses. Certainly it is tremendous value for money. At the outset, this news could come as a surprise, as till now, the Spoken Tutorial training programs were free of cost. But it is my belief that this move will be accepted in a positive way. 
-</p>
+<p>We wish to inform you that we have moved to a continual payment system wherein <b><u>Colleges get a full 12 months benefit from their date of payment. Eg. Jan 28th 2019 to Jan 27th 2020 etc.  </u></b> I would urge all colleges who are not in the program to come in and see what benefits they are getting for Rs.25000/-. There is NO OTHER course with this standard, this level of flexibilty and at this low cost per student.</p>
 
-<p><b>The Information Module & Payment Link on our website will be live in July</b>, and we will keep you informed. In the meanwhile - 
-<ol>
-    <li>Please discuss with your management Principal/ Director/ VC et al. about the policy change.</li>
-    <li>After approval/permission, please send us the name & contact details of the point person in your college who will access the payment link and take care of the financial transaction. This person will mostly be from the Accounts/Finance section of your college.</li>
-</ol></p>
-<p>Do drop a mail immediately to your respective state Training Coordinator, informing them of your college's decision, so we can proceed with the training. We guarantee our continued high quality services to all. If you have questions, don't hesitate to contact the Training Coordinator of your respective state.</p>
-<p>Yours Sincerely, <br><br>
+<p>One consideration from our end is related to the Test taking - Colleges who had done tests but were unable to download the certificates because colleges were disabled, we will activate those colleges for a short period of time, so that you can download the students certificates. For this you can contact your state training managers.</p>
 
-Shyama Iyer<br>
-National Coordinator - Training<br>
-Spoken Tutorial, IIT Bombay<br>
+<p>We guarantee our continued high-quality services to all. If you have questions, don't hesitate to contact the Training Coordinator of your respective state.</p>
+
+<p><b>Yours Sincerely, <br>
+Shyama Iyer <br>
+National Coordinator - Training Spoken Tutorial, IIT Bombay <br>
 NMEICT, MHRD, Govt. Of India<br>
-</p>
+</b></p>
 
 '''.format(organiser.user.email)
 
