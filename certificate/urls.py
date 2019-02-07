edb21c9fa4115e586cda6d5d6bdda67f727f1d42
kirti <kirti3192@gmail.com>
2019-02-07 10:45:52 +0530

    12 oct koha : rc certificate

--- certificate/urls.py
+++ certificate/urls.py
@@ -20,7 +20,9 @@ urlpatterns = patterns('certificate.views',  # noqa
         name='koha_workshop_download'),
     url(r'^koha_massive_workshop/$', 'koha_massive_workshop_download',
         name='koha_massive_workshop_download'),
-     url(r'^koha_coordinators_workshop/$', 'koha_coordinators_workshop_download', 
+    url(r'^koha_rc_certificate/$', 'koha_rc_certificate_download',
+        name='koha_rc_certificate_download'),
+    url(r'^koha_coordinators_workshop/$', 'koha_coordinators_workshop_download', 
         name='koha_coordinators_workshop_download'),
 
 )
