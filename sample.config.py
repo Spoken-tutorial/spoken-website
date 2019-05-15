abc057ef0926e7453f02952b9fa7ef5744ccdb47
shon <abhijitbonik@hotmail.com>
2019-05-15 11:51:02 +0530

    sample config file

---
+++ sample.config.py
@@ -0,0 +1,45 @@
+
+
+DB = 'spoken'
+DB_USER = 'root'
+DB_PASS = 'root'
+DB_HOST = '10.129.132.103'
+DB_PORT = '3305'
+
+MDB = 'moodle'
+MDB_USER = 'root'
+MDB_PASS = 'root'
+MDB_HOST = '10.129.132.103'
+MDB_PORT = '3306'
+
+CDB = 'cdeep'
+CDB_USER = 'root'
+CDB_PASS = 'root'
+CDB_HOST = '10.129.132.103'
+CDB_PORT = '3307'
+
+WDB = 'workshop'
+WDB_USER = 'root'
+WDB_PASS = 'root'
+WDB_HOST = '10.129.132.103'
+WDB_PORT = '3308'
+
+FDB = 'forum'
+FDB_USER = 'root'
+FDB_PASS = 'root'
+FDB_HOST = '10.129.132.103'
+FDB_PORT = '3309'
+
+SCRIPT_URL = ''
+STVIDEOS_PATH = ''
+ADMINISTRATOR_EMAIL = ''
+DEBUG_MODE = True
+ONLINE_TEST_URL = ''
+INDEX_PATH = ''
+TZ_STATUS = ''
+
+GOOGLE_RECAPTCHA_SITE_KEY = ''
+GOOGLE_RECAPTCHA_SECRET_KEY = ''
+GOOGLE_RECAPTCHA_SITEVERIFY = ''
+CHANNEL_KEY = ''
+HTML_MINIFY = ''
