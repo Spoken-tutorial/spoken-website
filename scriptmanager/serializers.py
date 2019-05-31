d803de312ea1686534c83d17d6abfc42fe8819a6
Rohit Prasad <rohitrp3@gmail.com>
2019-05-31 15:03:40 +0530

    Modify api to return foss category and langauge instead of ids

--- scriptmanager/serializers.py
+++ scriptmanager/serializers.py
@@ -1,7 +1,18 @@
-from creation.models import ContributorRole
+from creation.models import ContributorRole, FossCategory, Language
 from rest_framework import serializers
 
+class FossCategoryField(serializers.RelatedField):
+  def to_representation(self, value):
+    return value.foss
+
+class LanguageField(serializers.RelatedField):
+  def to_representation(self, value):
+    return value.name
+    
 class ContributorRoleSerializer(serializers.ModelSerializer):
+  foss_category = FossCategoryField(read_only=True)
+  language = LanguageField(read_only=True)
+
   class Meta:
     model = ContributorRole
     fields = ('foss_category', 'language', 'user', 'status')
