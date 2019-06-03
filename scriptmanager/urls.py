8396337d1fd2ede2bcab0b3b0ff79a5766b9c3d4
selvamanikannan <selvamanikannan55@gmail.com>
2019-06-03 18:08:03 +0530

    added tutorials list api

--- scriptmanager/urls.py
+++ scriptmanager/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
   url(r'^$', views.index, name='home'),
-  url(r'api/foss/', views.ContributorRoleList.as_view())
+  url(r'api/foss/', views.ContributorRoleList.as_view()),
+  url(r'api/tutorials/', views.TutorialsList.as_view())
 ]
--- scriptmanager/views.py
+++ scriptmanager/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from creation.models import ContributorRole
-from scriptmanager.serializers import ContributorRoleSerializer
+from creation.models import ContributorRole,TutorialDetail
+from scriptmanager.serializers import ContributorRoleSerializer,TutorialsList
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -24,4 +24,17 @@ class ContributorRoleList(generics.ListCreateAPIView):
 
   serializer_class = ContributorRoleSerializer
 
-  
+
+    
+
+class TutorialsList(generics.ListCreateAPIView):
+  serializer_class=TutorialsList
+  queryset = TutorialDetail.objects.all()
+
+  def get_queryset(self):
+        fid = self.request.query_params.get('fid')
+        if(fid=='all'):
+                return TutorialDetail.objects.all()
+        return TutorialDetail.objects.filter(foss_id=fid)
+
+
