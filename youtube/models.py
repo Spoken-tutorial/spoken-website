ea50c4ccc41a9c0fdf79af5e7a2511ae7a3bd27c
Ganesh Mohite <ganeshmohite96@gmail.com>
2019-06-03 18:00:42 +0530

    changes in youtube table

--- youtube/models.py
+++ youtube/models.py
@@ -2,9 +2,7 @@
 # Third Party Stuff
 from django.contrib.auth.models import User
 from django.db import models
-#from oauth2client.contrib.django_util import CredentialsField
-from oauthlib.oauth2 import ClientCredentialsGrant
 
 class CredentialsModel(models.Model):
     id = models.OneToOneField(User, primary_key=True, on_delete=models.PROTECT )
-    credential = ClientCredentialsGrant(User)
+    credential = models.TextField(null=True)
