4e1dc053dc29e95bc025eefbbc8dcad1e7357905
Rohit Prasad <rohitrp3@gmail.com>
2019-06-08 12:12:01 +0530

    Fix url issues with routing

--- scriptmanager/urls.py
+++ scriptmanager/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^$', views.index, name='home'),
   url(r'api/foss/(?P<fid>[0-9]+)/tutorials/', views.TutorialDetailList.as_view()), 
   url(r'api/foss/', views.ContributorRoleList.as_view()), 
   url(r'api/tutorial/(?P<tid>[0-9]+)/script/create/$', views.ScriptCreateAPIView.as_view()),
+  url(r'', views.index, name='home'),
 ]
