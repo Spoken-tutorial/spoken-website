62747648dc9bba568b68b5040852bc5702474605
mananpoddar <mananpoddarm@gmail.com>
2019-06-11 00:31:23 +0530

    Add Script View Component and resolved Issues in create Component

---
+++ scriptmanager/src/app/script/script-view/script-view.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CreateScriptService } from '../../_service/create-script.service';
+
+@Component({
+  selector: 'app-script-view',
+  templateUrl: './script-view.component.html',
+  styleUrls: ['./script-view.component.sass']
+})
+
+export class ScriptViewComponent implements OnInit {
+  public slides: any = [];
+  private id: number;
+  public foss;
+  constructor(
+    private route: ActivatedRoute,
+    public createscriptService: CreateScriptService
+  ) { }
+
+  public viewScript() {
+    this.createscriptService.getScript(
+      this.id
+    ).subscribe(
+      (res) => {
+          for(let i =0;i<res['length'];i++){
+            this.slides.push(res[i]);
+          }
+      },
+      console.error
+    );
+  }
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = +params['id'];
+    });
+    this.viewScript();
+  }
+
+}
