e2262386cad4fa0914b4b9a9f7c732f84d8d237a
selvamanikannan <selvamanikannan55@gmail.com>
2019-06-11 16:35:55 +0530

    added config file

---
+++ sample.config.py
@@ -0,0 +1,43 @@
+DB = 'spoken'
+DB_USER = 'root'
+DB_PASS = 'root'
+DB_HOST = 'localhost'
+DB_PORT = '3305'
+
+MDB = 'moodle'
+MDB_USER = 'root'
+MDB_PASS = 'root'
+MDB_HOST = 'localhost'
+MDB_PORT = '3306'
+
+CDB = 'cdeep'
+CDB_USER = 'root'
+CDB_PASS = 'root'
+CDB_HOST = 'localhost'
+CDB_PORT = '3307'
+
+WDB = 'workshop'
+WDB_USER = 'root'
+WDB_PASS = 'root'
+WDB_HOST = 'localhost'
+WDB_PORT = '3308'
+
+FDB = 'forum'
+FDB_USER = 'root'
+FDB_PASS = 'root'
+FDB_HOST = 'localhost'
+FDB_PORT = '3309'
+
+SCRIPT_URL = ''
+STVIDEOS_PATH = ''
+ADMINISTRATOR_EMAIL = ''
+DEBUG_MODE = True
+ONLINE_TEST_URL = ''
+INDEX_PATH = ''
+TZ_STATUS = True
+
+GOOGLE_RECAPTCHA_SITE_KEY = ''
+GOOGLE_RECAPTCHA_SECRET_KEY = ''
+GOOGLE_RECAPTCHA_SITEVERIFY = ''
+CHANNEL_KEY = ''
+HTML_MINIFY = ''
