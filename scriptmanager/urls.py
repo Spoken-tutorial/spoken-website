62cff531ecabd6175733db37871ea3238bc4f673
selvamanikannan <selvamanikannan55@gmail.com>
2019-07-10 11:54:15 +0530

    changes in revret api

--- scriptmanager/urls.py
+++ scriptmanager/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
   url(r'api/foss/(?P<fid>[0-9]+)/language/(?P<lid>[0-9]+)/tutorials/$', views.TutorialDetailList.as_view()), 
   url(r'api/foss/$', views.ContributorRoleList.as_view()), 
   url(r'api/tutorial/(?P<tid>[0-9]+)/language/(?P<lid>[0-9]+)/scripts/$', views.ScriptCreateAPIView.as_view()),
-  url(r'api/tutorial/(?P<tid>[0-9]+)/language/(?P<lid>[0-9]+)/scripts/(?P<script_detail_id>[0-9]+)/$', views.ScriptCreateAPIView.as_view()),
+  url(r'api/tutorial/(?P<tid>[0-9]+)/language/(?P<lid>[0-9]+)/scripts/(?P<script_detail_id>[0-9]+)/$', views.ScriptAPIView.as_view()),
   url(r'api/scripts/(?P<script_detail_id>[0-9]+)/comments/$', views.CommentCreateAPIView.as_view()),
   url(r'api/scripts/(?P<script_detail_id>[0-9]+)/reversions/$', views.ReversionListView.as_view()),
-  url(r'api/scripts/(?P<script_detail_id>[0-9]+)/reversions/(?P<reversion_id>[0-9]+)/$', views.ReversionListView.as_view()),
+  url(r'api/scripts/(?P<script_detail_id>[0-9]+)/reversions/(?P<reversion_id>[0-9]+)/$', views.ReversionRevertView.as_view()),
   url(r'api/docs/$', schema_view),
   url(r'', views.index, name='home')
 ]
