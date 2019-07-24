5ef0d93e4469c25172c3344f4d54c60063f64938
anjalysuresh <anjalysuresh@yahoo.co.in>
2019-07-24 17:01:02 +0530

    Update spoken.sh

--- spoken.sh
+++ spoken.sh
@@ -4,7 +4,7 @@ sudo apt-get install python3-dev libmysqlclient-dev
 pip3 install -r requirements-dev.txt
 pip3 install -r requirements-py3.txt
 
-pwd
+
 ls -al
 #cd Spoken tutorial script creation 
 
@@ -13,8 +13,8 @@ cd events
 cat > display.py
 chmod 777 display.py
 cd ..
-
-
+pwd
+ls -al
 
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
