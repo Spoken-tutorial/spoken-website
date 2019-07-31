99be9200b3eb80b236ade2c302b69ecf05d90ad7
Rohit Prasad <rohitrp3@gmail.com>
2019-08-01 02:45:09 +0530

    Add helper methods to decode jwt token

--- scriptmanager/src/app/_service/auth.service.ts
+++ scriptmanager/src/app/_service/auth.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  jwtHelper = new JwtHelperService();
+
   // using JwtToken for authorization purpose in angular system. Nothing to do with main django system.
   public getJwtToken() {
+    this.getToken();
+    this.isVideoReviewer();
     return this.http.post(
       'http://localhost:8000/api-token-auth/',
       {
@@ -15,6 +20,19 @@ export class AuthService {
         'password': 'test123'
       }
     );
+  }
+
+  public getToken() {
+    const token = localStorage.getItem('token');
+    console.log(token);
+    return token;
+  }
+
+  public isVideoReviewer() {
+    const token = this.getToken();
+    const decodedToken = this.jwtHelper.decodeToken(token);
+    console.log(decodedToken);
+    return true;
   }
   constructor(private http: HttpClient) { }
 }
