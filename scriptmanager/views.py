d285e550b21f7c9af935843d0b1eb1ffcf8a4b59
Rohit Prasad <rohitrp3@gmail.com>
2019-08-01 05:13:43 +0530

    Group languages of same foss category

--- scriptmanager/views.py
+++ scriptmanager/views.py
@@ -39,6 +39,34 @@ def index(request):
 class ContributorRoleList(generics.ListAPIView):
   def get_queryset(self):
       return ContributorRole.objects.filter(user = self.request.user,status = True)
+
+  def get(self, request):
+    categories = ContributorRoleSerializer(self.get_queryset(), many=True).data
+
+    unique_categories = {}
+    res = []
+
+    for category in categories:
+      category_id = category['foss_category']['id']
+
+      if category_id not in unique_categories:
+        foss_category = category['foss_category']
+        unique_categories[category_id] = {
+          'foss_category': {
+            'id': foss_category['id'],
+            'name': foss_category['name'],
+            'description': foss_category['description']
+          },
+          'languages': []
+        }
+
+      unique_categories[category_id]['languages'].append(category['language'])
+
+    for (k,v) in unique_categories.items():
+      res.append(v)
+
+    return Response({ 'data': res }, status=200)
+
   serializer_class  =  ContributorRoleSerializer
 
 
