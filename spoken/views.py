c2051a6aea6aafdd1cee5f7498ee1b75f7e1357c
kirti <kirti3192@gmail.com>
2019-09-18 11:05:34 +0530

    new page to display archived fosses

--- spoken/views.py
+++ spoken/views.py
@@ -236,6 +236,67 @@ def series_tutorial_search(request):
     context['current_foss'] = foss_get
     return render(request, 'spoken/templates/series_tutorial_search.html', context)
 
+def archived_foss(request):
+    '''
+    Get all the media testimonials which are set to not 
+    show on home page and display along the form to display the tutorials.
+    '''
+    form = ArchivedTutorialSearchForm()
+    collection = None
+    # # Get all the video / audio testimonials in series
+    # foss_list = TutorialResource.objects.filter(Q(status=1) | Q(status=2), language__name='English', tutorial_detail__foss__show_on_homepage = 0).values_list('tutorial_detail__foss__id').annotate().distinct()
+    # collection =  MediaTestimonials.objects.filter(foss__id__in=foss_list).values("foss__foss", "content", "created", "foss", "foss_id", "id", "path", "user", "workshop_details").order_by('-created')
+    
+    # if collection:
+    #     page = request.GET.get('page')
+    #     collection = get_page(collection, page, limit=6)
+    
+    # add_button_show= False
+    # if request.user.has_perm('events.add_testimonials'):
+        # add_button_show= True
+    context = {}
+    context['form'] = form
+    # context['collection'] = collection
+    # context['media_url'] = settings.MEDIA_URL
+    # context['add_button_show'] = add_button_show
+    return render(request, 'spoken/templates/archived_foss_list.html', context)
+
+@csrf_exempt
+def archived_tutorial_search(request):
+    context = {}
+    collection = None
+    form = ArchivedTutorialSearchForm()
+    foss_get = ''
+    show_on_homepage = 2
+    queryset = TutorialResource.objects.filter(Q(status=1) | Q(status=2), tutorial_detail__foss__show_on_homepage = show_on_homepage)
+    
+    if request.method == 'GET' and request.GET:
+        form = ArchivedTutorialSearchForm(request.GET)
+        if form.is_valid():
+            foss_get = request.GET.get('search_otherfoss', '')
+            language_get = request.GET.get('search_otherlanguage', '')
+            if foss_get and language_get:
+                collection = queryset.filter(tutorial_detail__foss__foss=foss_get, language__name=language_get).order_by('tutorial_detail__level', 'tutorial_detail__order')
+
+            elif foss_get:
+                collection = queryset.filter(tutorial_detail__foss__foss=foss_get).order_by('tutorial_detail__level', 'tutorial_detail__order', 'language__name')
+            elif language_get:
+                collection = queryset.filter(language__name=language_get).order_by('tutorial_detail__foss__foss', 'tutorial_detail__level', 'tutorial_detail__order')
+            else:
+                collection = queryset.filter(tutorial_detail__foss__id__in=FossCategory.objects.values('id'), language__id__in=Language.objects.values('id')).order_by('tutorial_detail__foss__foss', 'language__name', 'tutorial_detail__level', 'tutorial_detail__order')
+    else:
+        foss = queryset.filter(language__name='English').values('tutorial_detail__foss__foss').annotate(Count('id')).values_list('tutorial_detail__foss__foss').distinct().order_by('?')[:1].first()
+        collection = queryset.filter(tutorial_detail__foss__foss=foss[0], language__name='English')
+        foss_get = foss[0]
+    if collection:
+        page = request.GET.get('page')
+        collection = get_page(collection, page)
+    context['form'] = form
+    context['collection'] = collection
+    context['SCRIPT_URL'] = settings.SCRIPT_URL
+    context['current_foss'] = foss_get
+    return render(request, 'spoken/templates/archived_tutorial_search.html', context)
+
 
 def watch_tutorial(request, foss, tutorial, lang):
     try:
