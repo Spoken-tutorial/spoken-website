25276191eed3db712e1736e885233c9327ac520b
abhisgithub <abhijitbonik@hotmail.com>
2019-11-14 17:24:52 +0530

    cdcontent added to logs

--- logs/urls.py
+++ logs/urls.py
@@ -5,6 +5,10 @@ EVENT_NAME_DICT={
     r'^/watch/([0-9a-zA-Z-+%\(\)., ]+)/([0-9a-zA-Z-+%\(\)., ]+)/([a-zA-Z-]+)/$' : {
         'name' : 'event.video.watch'
     },
+
+    r'^/cdcontent/$' : {
+        'name' : 'event.content.download'
+    },
     
     'home':{
         'name' : 'event.home.view'
