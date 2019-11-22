e8b12ac873247870f380eb03078e39e862eebd00
firuza <firya87@gmail.com>
2019-11-22 15:18:24 +0530

    permission: contributor and reviewer for FOSS and language

--- scriptmanager/permissions.py
+++ scriptmanager/permissions.py
@@ -1,13 +1,15 @@
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from creation.views import is_domainreviewer, is_qualityreviewer
+from creation.models import TutorialDetail, ContributorRole, DomainReviewerRole, QualityReviewerRole
 
 class ScriptOwnerPermission(IsAuthenticatedOrReadOnly):
   def has_object_permission(self, request, view, obj):
     user = request.user
+    tutorialdetails = getFOSS(obj.tutorial)    
     
     if (obj.status): return True
 
-    if (not user.is_anonymous() and (is_domainreviewer(user) or is_qualityreviewer(user) or obj.user == user)):
+    if (not user.is_anonymous() and (isDomainReviewer(tutorialdetails, obj.language, user) or isQualityReviewer(tutorialdetails, obj.language, user) or isContributor(tutorialdetails, obj.language, user))):
       return True
     
     return False
@@ -15,10 +17,11 @@ class ScriptOwnerPermission(IsAuthenticatedOrReadOnly):
 class ScriptModifyPermission(IsAuthenticatedOrReadOnly):
   def has_object_permission(self, request, view, obj):
     user = request.user
+    tutorialdetails = getFOSS(obj.script.tutorial)
     
     if (user.is_anonymous() or obj.script.status): return False
 
-    return is_domainreviewer(user) or is_qualityreviewer(user) or obj.script.user == user
+    return (isDomainReviewer(tutorialdetails, obj.script.language, user) or isQualityReviewer(tutorialdetails, obj.script.language, user) or isContributor(tutorialdetails, obj.script.language, user))
       
 class PublishedScriptPermission(IsAuthenticatedOrReadOnly):
   def has_object_permission(self, request, view, obj):
@@ -46,3 +49,22 @@ class CommentOwnnerPermission(IsAuthenticatedOrReadOnly):
       return is_domainreviewer(user) or is_qualityreviewer(user) or obj.user == user
 
     return request.user == obj.user
+
+def getFOSS(tutorial):
+  tutorialdetails = TutorialDetail.objects.get(tutorial=tutorial)
+  return tutorialdetails
+
+def isContributor(foss, language, user):
+  if ContributorRole.objects.filter(foss_category=foss.foss, language=language, user=user).exists():
+    return True
+  return False
+
+def isDomainReviewer(foss, language, user):
+  if DomainReviewerRole.objects.filter(foss_category=foss.foss, language=language, user=user).exists():
+    return True
+  return False
+
+def isQualityReviewer(foss, language, user):
+  if QualityReviewerRole.objects.filter(foss_category=foss.foss, language=language, user=user).exists():
+    return True
+  return False
