afd04a2f64b55b0f0ddce6c20a44d967ad038fbf
firuza <firya87@gmail.com>
2020-02-05 19:07:35 +0530

    added support for superscript, subscript, hyperlinks, and images in the script

--- scriptmanager/src/app/app.module.ts
+++ scriptmanager/src/app/app.module.ts
@@ -85,13 +85,15 @@ export function tokenGetter() {
           // ['blockquote', 'code-block'],
 
           [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-          // [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+          [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
           // [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
 
           // [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
           [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
           // [{ 'font': [] }],
+
+          ['link', 'image'],
 
           ['clean'],                                         // remove formatting button
         ]
