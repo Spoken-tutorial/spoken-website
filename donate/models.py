bc8a04503ba65ed1caa025b71441615fb8cfc5bd
root <root@localhost.localdomain>
2020-06-10 02:30:09 -0400

    models update

--- donate/models.py
+++ donate/models.py
@@ -9,19 +9,20 @@ class Payment(models.Model):
     email = models.EmailField(max_length=255)
     country = models.CharField(max_length=50,null=True)
     state = models.CharField(max_length=50,null=True)
-    #city =  models.CharField(max_length=75,null=True)
+    city =  models.CharField(max_length=75,null=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=6)
-    amount = models.DecimalField(max_digits=5,decimal_places=2)
+    amount = models.DecimalField(max_digits=10,decimal_places=2)
     key = models.UUIDField(default=uuid.uuid4, editable=False)
     status = models.BooleanField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    expiry = models.DateTimeField(blank=True, null=True)
     user = models.ForeignKey(User,on_delete=models.PROTECT,related_name="payment_user" )
  
-class FossLangDict(models.Model):
+class CdFossLanguages(models.Model):
     payment = models.ForeignKey(Payment, on_delete=models.PROTECT)
     foss = models.ForeignKey(FossCategory, on_delete=models.PROTECT,related_name="payment_foss")
-    lang = models.ManyToManyField(Language)
+    lang = models.ForeignKey(Language)
 
 class PaymentTransaction(models.Model):
     paymentdetail = models.ForeignKey(Payment, on_delete=models.PROTECT, related_name="payment_transaction" )
