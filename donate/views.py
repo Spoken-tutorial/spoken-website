659ef7202c158b33f05eeb7307e45d30f2ceb296
MetalBlazer <adhikarysaurabh@gmail.com>
2020-07-08 16:25:53 +0530

    updates

--- donate/views.py
+++ donate/views.py
@@ -124,8 +124,8 @@ def calculate_expiry():
 def encrypted_data(request, form):
     STdata = ''
     user_name = form.cleaned_data.get('name')
-    #amount = form.cleaned_data.get('amount')   
-    amount = 1.00
+    amount = form.cleaned_data.get('amount')   
+    #amount = 1.00
     purpose = PURPOSE + str(form.save(commit=False).pk)
     STdata = str(request.user.id) + str(user_name) + str(amount) + purpose + CHANNEL_ID + CHANNEL_KEY
     
@@ -139,8 +139,8 @@ def get_final_data(request, form):
     data = {
         'userId': request.user.id,
         'name': form.cleaned_data.get('name'),
-        #    'amount':form.cleaned_data.get('amount'),
-        'amount': 1.00,
+        'amount':form.cleaned_data.get('amount'),
+        #'amount': 1.00,
         'purpose': PURPOSE + str(form.save(commit=False).pk),
         'channelId': CHANNEL_ID,
         'target': TARGET,
