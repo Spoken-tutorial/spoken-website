aac2e2c8fd9885880708ca8862486ab9d55dd82b
abhijitbonik <abhijitbonik@hotmail.com>
2020-09-19 00:05:26 +0530

    logging management command

--- creation/management/commands/generate_foss_metadata.py
+++ creation/management/commands/generate_foss_metadata.py
@@ -7,8 +7,9 @@ from django.db.models import Q
 import csv
 
 class Command(BaseCommand):
-    
+        
     def handle(self, *args, **options):
+        print("Generating Foss Metadata. Please wait...")
         meta_file_name = 'metadata_'+uuid.uuid4().hex+".csv"
         with open(settings.MEDIA_ROOT + meta_file_name, "w+", newline='') as metafile:
             metawriter = csv.writer(metafile)
@@ -31,14 +32,15 @@ class Command(BaseCommand):
                         pass
                 deeplink_url="https://spoken-tutorial.org/tutorial-search/?search_foss={}&search_language={}".format(f.foss, 'English')
                 wiki_url = "https://script.spoken-tutorial.org/index.php/{}".format(f.foss.replace(" ", "_"))
-                metadata = [str(f.id), f.foss, self.convert(course_duration), deeplink_url, wiki_url, f.description, ", ".join(keywords), 'English', str(tr_en.count())]
-                metawriter.writerow(metadata)
+                if tr_en.count() >= 1:
+                    metadata = [str(f.id), f.foss, self.convert(course_duration), deeplink_url, wiki_url, f.description, ", ".join(keywords), 'English', str(tr_en.count())]
+                    metawriter.writerow(metadata)
                 
                 languages = Language.objects.all().exclude(name='English')
                 for l in languages:
                     tr = TutorialResource.objects.filter(Q(status=1) | Q(status=2),tutorial_detail__foss=f, language=l)
                     deeplink_url="https://spoken-tutorial.org/tutorial-search/?search_foss={}&search_language={}".format(f.foss, l.name)
-                    if tr.count() == tr_en.count():
+                    if tr.count() == tr_en.count() and tr.count() >=1:
                         metadata = [str(f.id), f.foss, self.convert(course_duration), deeplink_url, wiki_url, f.description, ", ".join(keywords), l.name, str(tr.count())]
                         metawriter.writerow(metadata)
                     else:
@@ -46,8 +48,11 @@ class Command(BaseCommand):
                             video_path = settings.MEDIA_ROOT+'videos/'+str(t.tutorial_detail.foss.pk)+'/'+str(t.tutorial_detail.pk)+'/'+t.video
                             video_info = get_video_info(video_path)
                             course_duration += video_info['total']
-                        metadata = [str(f.id), f.foss, self.convert(course_duration), deeplink_url, wiki_url, f.description, ", ".join(keywords), l.name, str(tr_en.count())]
-                        metawriter.writerow(metadata)
+                        if tr.count() >= 1:
+                            metadata = [str(f.id), f.foss, self.convert(course_duration), deeplink_url, wiki_url, f.description, ", ".join(keywords), l.name, str(tr.count())]
+                            metawriter.writerow(metadata)
+            print("Metadata File Generated. Please find the file at location given below.")
+            print(settings.MEDIA_ROOT + metafile.name)
 
     def convert(self, seconds): 
         seconds = seconds % (24 * 3600) 
