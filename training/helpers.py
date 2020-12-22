78c8696283afb3a781b2d6523d072d099869f2cf
kirti <kirti3192@gmail.com>
2020-12-22 22:42:47 +0530

    export as csv work done

--- training/helpers.py
+++ training/helpers.py
@@ -1,5 +1,6 @@
-from events.models import AcademicKey, StudentBatch
-from training.models import TrainingEvents
+from events.models import *
+from training.models import *
+from donate.models import *
 from datetime import datetime,date
 from certificate.views import _clean_certificate_certificate
 from django.http import HttpResponse
@@ -102,3 +103,102 @@ def create_certificate(eventid,pname):
         return response
     except Exception as e:
         print("error is ",e)
+
+
+def get_transaction_details(request, purpose):
+    user = User.objects.get(id=request.user.id)
+    rp_states = ResourcePerson.objects.filter(status=1,user=user)
+    state = State.objects.filter(id__in=rp_states.values('state')).values('name')
+
+    get_state = ''
+    allpaydetails = ''
+
+    if purpose == 'cdcontent':
+            allpaydetails = PaymentTransaction.objects.filter(paymentdetail__purpose='cdcontent', paymentdetail__state__in=state).order_by('-created')
+        
+    else:
+        rp_events = TrainingEvents.objects.filter(state__name__in = state)
+        allpaydetails = PaymentTransaction.objects.filter(paymentdetail__purpose__in=rp_events).exclude(paymentdetail__purpose='cdcontent').order_by('-created')
+
+    
+    
+    selected_state = request.GET.get('state')
+    academic_center = request.GET.get('college')
+    selected_event = request.GET.get('events')
+    status = request.GET.get('status')
+    request_type = request.GET.get('request_type')
+    email=request.GET.get('user_email')
+    fdate = request.GET.get('fdate')
+    tdate = request.GET.get('tdate')
+
+    if selected_state:
+        get_state = State.objects.filter(id=selected_state).values('name')
+
+    if academic_center in ('None','0',0):
+        academic_center = False
+    else:
+        academic_center = request.GET.get('college')
+
+    if selected_event in ('None','0',0):
+        selected_event = False
+
+    else:
+        selected_event = request.GET.get('events')
+    
+
+    if purpose == 'cdcontent':
+        if get_state:
+            paymentdetails=allpaydetails.filter(paymentdetail__state=get_state)
+        else:
+            paymentdetails=allpaydetails.filter(paymentdetail__state__in=state)
+        if request.GET.get('user_email'):
+            email=request.GET.get('user_email')
+            paymentdetails = paymentdetails.filter(paymentdetail__email=email).order_by('-created')
+
+    else:#purpose event
+        # allpaydetails = PaymentTransaction.objects.filter().exclude(paymentdetail__purpose='cdcontent').order_by('-created')
+
+        if get_state:
+            academic_centers = AcademicCenter.objects.filter(state__name=get_state)
+
+            if academic_center:
+                events = TrainingEvents.objects.filter(host_college = academic_center)
+                if selected_event:
+                    e = TrainingEvents.objects.get(id=selected_event)
+                    paymentdetails = allpaydetails.filter(paymentdetail__purpose=e.id)
+                else:
+                    paymentdetails = allpaydetails.filter(paymentdetail__purpose__in=events)
+
+            else:
+                events = TrainingEvents.objects.filter(host_college__in = academic_centers)
+                paymentdetails = allpaydetails.filter(paymentdetail__purpose__in=events)
+        else:
+            academic_centers = AcademicCenter.objects.filter(state__name__in=state)
+            events = TrainingEvents.objects.filter(host_college__in = academic_centers)
+            
+            paymentdetails = allpaydetails.filter(paymentdetail__purpose__in=events)
+        if request.GET.get('user_email'):
+            email=request.GET.get('user_email')
+            paymentdetails = paymentdetails.filter(paymentdetail__email=email).order_by('-created')
+            
+
+
+    allpaydetails = paymentdetails
+
+    if status and status in ('S','F','X'):
+        
+        allpaydetails = allpaydetails.filter(status=status)
+
+    if request_type:
+        if request_type == 'R':
+            allpaydetails = allpaydetails.filter(requestType='R')
+        elif request_type == 'I':
+            allpaydetails = allpaydetails.filter(requestType='I')
+
+    if request.GET.get('fdate'):
+        if request.GET.get('tdate'):
+            allpaydetails = allpaydetails.filter(Q(created__gt=request.GET.get('fdate')) & Q(created__lt= request.GET.get('tdate')))
+        else:
+            allpaydetails = allpaydetails.filter(created__gt=request.GET.get('fdate'))
+
+    return allpaydetails
