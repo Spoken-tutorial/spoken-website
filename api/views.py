0fd0908be57643c72331c6cc2d4db7aadad02433
ankitamk14 <ankitamk@gmail.com>
2021-10-11 12:35:34 +0530

    added menu items (#423)
    * added menu items
    
    * fix - foss list api for H&N
    
    * fix - foss list api for H&N

--- api/views.py
+++ api/views.py
@@ -147,7 +147,7 @@ def get_schoolfosslist(request):
     List all fosses for school website.
     """
     if request.method == 'GET':
-        fosses = FossCategory.objects.filter(status=1, available_for_nasscom=1)
+        fosses = FossCategory.objects.filter(status=1)
         serializer = FossSerializer(fosses, many=True)
         return JsonResponse(serializer.data, safe=False)
 
