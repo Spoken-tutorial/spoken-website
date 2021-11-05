390d0cfc9eeef9ef4cfd946792f3b35ec9f68e59
Kirti <kirti3192@gmail.com>
2021-11-05 21:41:58 +0530

    language error fix health cd content

--- cdcontent/forms.py
+++ cdcontent/forms.py
@@ -48,7 +48,7 @@ class CDContentForm(forms.Form):
                     except:
                         tmp_level = ''
                     if tmp_level:
-                        lang_recs = list(TutorialResource.objects.filter(Q(status = 1)|Q(status = 2), tutorial_detail__foss__show_on_homepage = 1, tutorial_detail__foss_id = int(args[0]['foss_category']), tutorial_detail__level_id = int(tmp_level)).values_list('language_id', 'language__name').order_by('language__name').distinct())
+                        lang_recs = list(TutorialResource.objects.filter(Q(status = 1)|Q(status = 2), tutorial_detail__foss_id = int(args[0]['foss_category']), tutorial_detail__level_id = int(tmp_level)).values_list('language_id', 'language__name').order_by('language__name').distinct())
                     else:
-                        lang_recs = list(TutorialResource.objects.filter(Q(status = 1)|Q(status = 2), tutorial_detail__foss__show_on_homepage = 1, tutorial_detail__foss_id = int(args[0]['foss_category'])).values_list('language_id', 'language__name').order_by('language__name').distinct())
+                        lang_recs = list(TutorialResource.objects.filter(Q(status = 1)|Q(status = 2), tutorial_detail__foss_id = int(args[0]['foss_category'])).values_list('language_id', 'language__name').order_by('language__name').distinct())
                     self.fields['language'].choices = lang_recs
