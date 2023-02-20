3ae3540ce24c5e22a80cc13e3e6acf1e861dc69e
ankitamk14 <ankitamk@gmail.com>
2023-02-20 10:46:41 +0530

    Added boolean field to Foss Category (#457)
    * added boolean field to Foss Category
    
    * added verbose name
    
    * modification for single migration file
    
    * Update creation/models.py
    
    argument keyword
    
    Co-authored-by: Sunil Shetye <37403318+sunilshetye@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sunil Shetye <37403318+sunilshetye@users.noreply.github.com>

--- creation/models.py
+++ creation/models.py
@@ -89,7 +89,8 @@ class FossCategory(models.Model):
     available_for_nasscom = models.BooleanField(default=True, help_text ='If unchecked, this foss will not be available for nasscom' )
     available_for_jio = models.BooleanField(default=True, help_text ='If unchecked, this foss will not be available for jio, csc and spoken-tutorial.in' )
     csc_dca_programme = models.BooleanField(default=True, help_text ='If unchecked, this foss will not be available for csc-dca programme' )
-    credits = models.PositiveSmallIntegerField(default=0) 
+    credits = models.PositiveSmallIntegerField(default=0)
+    is_fossee = models.BooleanField(verbose_name="Added by FOSSEE", default=False)
     class Meta(object):
         verbose_name = 'FOSS'
         verbose_name_plural = 'FOSSes'
