f1be179f21264796e35c37ad7142c7a6a2135989
ankitamk14 <ankitamk@gmail.com>
2024-04-04 18:38:27 +0530

    modified order of columns

--- training/admin.py
+++ training/admin.py
@@ -5,7 +5,7 @@ from .models import ILWFossMdlCourses, TrainingEvents
 
 
 class TrainingEventsAdmin(admin.ModelAdmin):
-    list_display = ('event_name', 'event_type', 'event_fee', 'state', 'host_college', 'foss', 'Language_of_workshop', 'event_start_date', 'event_end_date', 'event_coordinator_name', 'event_coordinator_email', 'event_coordinator_contact_no', 'registartion_start_date', 'registartion_end_date', 'training_status', 'entry_date', 'entry_user')
+    list_display = ('event_name', 'entry_user', 'event_type', 'event_fee', 'state', 'host_college', 'foss', 'Language_of_workshop', 'event_start_date', 'event_end_date', 'event_coordinator_name', 'event_coordinator_email', 'event_coordinator_contact_no', 'registartion_start_date', 'registartion_end_date', 'training_status', 'entry_date')
     list_filter = ('event_type', 'state',  'foss', 'Language_of_workshop', 'event_start_date', 'event_end_date', 'training_status')
     search_fields = ('event_name', 'event_coordinator_name', 'event_coordinator_email', 'event_coordinator_contact_no')
     ordering = ('-entry_date',)
