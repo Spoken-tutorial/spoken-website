785c383dd31ead2fd2cf0c87ceb1ffa1f3e65bc4
ankitamk14 <ankitamk@gmail.com>
2024-04-26 12:04:30 +0530

    query optimization (#493)
    * query optimization
    
    * query modification

--- statistics/views.py
+++ statistics/views.py
@@ -78,7 +78,7 @@ def get_state_info(request, code):
         print(e)
         return HttpResponse('<h4 style="margin: 30px;">Permission Denied!</h4>')
 
-
+import time
 def training(request):
     """ Organiser index page """
     collectionSet = TrainingRequest.objects.filter(
@@ -153,10 +153,13 @@ def training(request):
     malecount = cache.get(male_key)
     if status != 0:
         if not femalecount or not malecount:
-            female_list=list(Student.objects.filter(trainingattend__training_id__in=[col.id for col in collection.qs], gender='Female').values_list('id'))
-            femalecount= len([i[0] for i in female_list])
-            male_list=list(Student.objects.filter(trainingattend__training_id__in=[col.id for col in collection.qs], gender='Male').values_list('id'))
-            malecount= len([i[0] for i in male_list])
+            training_ids = [col.id for col in collection.qs]
+            gender_counts = TrainingAttend.objects.filter(training_id__in=training_ids).values('student__gender').annotate(gender_count=Count('student__gender'))
+            for item in gender_counts:
+                if item['student__gender'] == 'Female':
+                    femalecount = item['gender_count']
+                if item['student__gender'] == 'Male':
+                    malecount = item['gender_count']
             try:
                 cache.set(female_key, femalecount)
                 cache.set(male_key, malecount)
