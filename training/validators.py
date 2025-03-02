41caf7419dd4fe19cbed5aed921fb253edd8b1f3
ankitamk14 <ankitamk@gmail.com>
2025-03-02 23:49:09 +0530

    removing validate_email

--- training/validators.py
+++ training/validators.py
@@ -30,6 +30,7 @@ def validate_csv_file(csv_file):
                 line+=1
                 continue
             data = csv.reader([row.decode('utf-8')], delimiter=',', quotechar='|')
+            
             for col in data:
                 try:
                     ASCIIValidator()(col[0].strip())
@@ -42,8 +43,6 @@ def validate_csv_file(csv_file):
                 try:
                     EmailValidator()(col[2].strip())
                     ASCIIUsernameValidator()(col[2].strip())
-                    if not validate_email(col[2].strip(), verify=True):
-                        error.update(dict(lname="Email: "+ col[2] +" has errors."))
                 except:
                     error.update(dict(lname="Email: "+ col[2] +" has errors."))
                 try:
