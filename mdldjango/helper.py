64aa86078e534682ad27d2bd23c0bf4e3cb94f14
ankitamk14 <ankitamk@gmail.com>
2025-10-17 15:01:02 +0530

    Test attendance record bug fix

--- mdldjango/helper.py
+++ mdldjango/helper.py
@@ -33,6 +33,7 @@ def get_moodle_user(academic_id, firstname, lastname, gender, email):
             mdluser.password = password"""
         mdluser.save()
     except Exception as e:
+      print(f"No moodle user found : {e} - {email}\033[0m")
       try:
         mdluser = MdlUser()
         mdluser.auth = 'manual'
@@ -45,44 +46,9 @@ def get_moodle_user(academic_id, firstname, lastname, gender, email):
         mdluser.confirmed = 1
         mdluser.mnethostid = 1
         mdluser.save()
-        mdluser = MdlUser.objects.filter(email = email, firstname= firstname, username=username, password=password).first()
-
-        # send password to email
-        subject  = "Spoken Tutorial Online Test password"
-        to = [mdluser.email]
-        message = '''Hi {0},
-
-Your account password at 'Spoken Tutorials Online Test as follows'
-
-Your current login information is now:
-username: {1}
-password: {2}
-
-Please go to this page to change your password:
-{3}
-
-In most mail programs, this should appear as a blue link
-which you can just click on.  If that doesn't work,
-then cut and paste the address into the address
-line at the top of your web browser window.
-
-Cheers from the 'Spoken Tutorials Online Test Center' administrator,
-
-Admin Spoken Tutorials
-'''.format(mdluser.firstname, mdluser.username, password_string, 'http://onlinetest.spoken-tutorial.org/login/change_password.php')
-
-        # send email
-        email = EmailMultiAlternatives(
-            subject, message, 'administrator@spoken-tutorial.org',
-            to = to, bcc = [], cc = [],
-            headers={'Reply-To': 'no-replay@spoken-tutorial.org', "Content-type":"text/html;charset=iso-8859-1"}
-        )
-        try:
-            result = email.send(fail_silently=False)
-        except:
-            pass
-      except:
-        return MdlUser.objects.filter(email = email).first()
+        mdluser = MdlUser.objects.get(email=email)
+      except Exception as e:
+        print(f"exception during mdl user creation : {e} \033[0m")
     return mdluser
 
 
