b2bdfdd28e68399a7fe0fb3e5d55f3b29f11f657
ayishanishana21 <86144493+ayishanishana21@users.noreply.github.com>
2026-01-07 13:07:42 +0530

    filter in ILW Event (#636)
    * filter in ILW Event
    
    * removed filter

--- training/filters.py
+++ training/filters.py
@@ -1,7 +1,14 @@
 import django_filters
 from .models import Company
+from training.models import Participant
+from creation.models import FossCategory
+from events.models import State, AcademicCenter
+from training.models import TrainingEvents, Participant
+
 
 class CompanyFilter(django_filters.FilterSet):
     class Meta:
         model = Company
         fields = ['name', 'state', 'company_type']
+
+
