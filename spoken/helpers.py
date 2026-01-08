9d8241e7210c5691a2bd118dac3eb759782a4e6f
ayishanishana21 <86144493+ayishanishana21@users.noreply.github.com>
2026-01-08 12:50:16 +0530

    Fix/cache resolution (#639)
    * resolved cache
    
    * Add cache registry cache-related templates

--- spoken/helpers.py
+++ spoken/helpers.py
@@ -9,6 +9,7 @@ from django.db.models import Q, Count
 from creation.models import TutorialSummaryCache, TutorialResource, FossCategory
 from events.models import Testimonials
 from cms.models import Notification, Event
+from cms.cache_registry import register_cache_key
 from .config import CACHE_RANDOM_TUTORIALS, CACHE_TR_REC, CACHE_TESTIMONIALS, CACHE_NOTIFICATIONS, CACHE_EVENTS, CACHE_TUTORIALS
 
 def get_key(identifier, key_val):
@@ -141,8 +142,8 @@ def get_foss_choice(show_on_homepage=1, lang=None):
             foss_list_choices.append((str(foss_row[0]), str(foss_row[0]) + ' (' + str(foss_row[1]) + ')'))
 
     cache.set(cache_key, foss_list_choices, timeout=CACHE_TUTORIALS)
+    register_cache_key(cache_key)
     return foss_list_choices
-
 
 
 # ----  Language Choice For Search Bar ----
@@ -166,4 +167,5 @@ def get_lang_choice(show_on_homepage=1, foss=None):
         lang_list_choices.append((str(lang_row[0]), str(lang_row[0]) + ' (' + str(lang_row[1]) + ')'))
 
     cache.set(cache_key, lang_list_choices, timeout=CACHE_TUTORIALS)
+    register_cache_key(cache_key)
     return lang_list_choices
