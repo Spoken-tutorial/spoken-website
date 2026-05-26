e382bdf4647222471c093cc337793bd173ec486e
ankitamk14 <ankitamk@gmail.com>
2026-05-26 23:45:49 +0530

    bug fix

--- mdldjango/templatetags/mdldata.py
+++ mdldjango/templatetags/mdldata.py
@@ -46,14 +46,15 @@ def get_moodle_courseid(rid, mdluser_id):
     #print "mdluser =>", mdluser_id
     #ensure the training.fossmdlmap_id.foss == training.courseMap.foss
     training = Test.objects.get(id=rid).training
-    fmap_foss = training.fossmdlmap.foss_id
-    cmap_foss = training.course.foss_id
-    if fmap_foss != cmap_foss:
-        print(f"\033[91m NOT EQUAL : {fmap_foss}  {cmap_foss}\033[0m")
-        # fossmdlmap = FossMdlCourses.objects.filter(foss_id = cmap_foss, level="Advanced", language="English")
-        fossmdlmap = FossMdlCourses.objects.filter(foss_id = cmap_foss).first()
-        training.fossmdlmap_id = fossmdlmap.id
-        training.save()
+    if training.fossmdlmap is not None:
+        fmap_foss = training.fossmdlmap.foss_id
+        cmap_foss = training.course.foss_id
+        if fmap_foss != cmap_foss:
+            print(f"\033[91m NOT EQUAL : {fmap_foss}  {cmap_foss}\033[0m")
+            # fossmdlmap = FossMdlCourses.objects.filter(foss_id = cmap_foss, level="Advanced", language="English")
+            fossmdlmap = FossMdlCourses.objects.filter(foss_id = cmap_foss).first()
+            training.fossmdlmap_id = fossmdlmap.id
+            training.save()
     try:
         wa = TestAttendance.objects.get(test_id = rid, mdluser_id = mdluser_id)
         #print wa.mdlcourse_id
