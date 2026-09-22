47098a14ccb3dc5959c5eb3dc6171ef1df835920
ayishanishana21 <ayishanishana21@gmail.com>
2026-09-22 21:49:38 +0530

    fixed org_training error

--- events/certificates.py
+++ events/certificates.py
@@ -160,7 +160,9 @@ def get_test_cert_text(test, mdluser, credits=''):
    academic = test.academic
    academic_code = academic.academic_code
    institution = test.academic.institution_name
-   organization = get_organization(test.training.training_start_date)
+   # organization = get_organization(test.training.training_start_date)
+   org_course_material = "EduPyramids, SINE, IIT Bombay"
+   org_training = "This training is offered through SWAYAM Plus by EduPyramids, SINE, IIT Bombay"
    organizer = f"{test.organiser.user.first_name} {test.organiser.user.last_name}"
    invigilator = f"{test.invigilator.user.first_name} {test.invigilator.user.last_name}"
    text_end = f"{org_training}"
@@ -184,9 +186,9 @@ def get_test_cert_text(test, mdluser, credits=''):
    
    #paragraphe
    if test.training.department.id == FDP:
-      text = f"This is to certify that <b>{name}</b> has successfully completed <b>{foss}</b> test on <b>{test_date}</b> organized at <b>{institution}</b> by <b>{organizer}</b> with course material provided by {organization}. Passing an online exam, conducted remotely from IIT Bombay, is a pre-requisite for completing this Faculty Development Programme.<br/><br/><b>{invigilator}</b> at <b>{institution}</b> invigilated this examination. {text_end}.</p><br /><br />{credits}"
-   elif test.academic.institution_type_id == CSC: # CHECK #TODO
-      text = f"This is to certify that <b>{name}</b> has successfully completed <b>{foss}</b> test organized at {institution} by <u>{organizer}</u> with course material provided by {organization}. Passing an online exam, conducted remotely from IIT Bombay, is a pre-requisite for completing this training. <u>{invigilator}</u> at {institution} invigilated this examination.<br/>This training is offered by the Spoken Tutorial Project, IIT Bombay, funded by National Mission on Education through ICT, Ministry of Education, Govt., of India."
+      text = f"This is to certify that <b>{name}</b> has successfully completed <b>{foss}</b> test on <b>{test_date}</b> organized at <b>{institution}</b> by <b>{organizer}</b> with course material provided by {org_course_material}. Passing an online exam, conducted remotely from IIT Bombay, is a pre-requisite for completing this Faculty Development Programme.<br/><br/><b>{invigilator}</b> at <b>{institution}</b> invigilated this examination. {text_end}.</p><br /><br />{credits}"
+   elif test.academic.institution_type_id == CSC: # CHECK 
+      text = f"This is to certify that <b>{name}</b> has successfully completed <b>{foss}</b> test organized at {institution} by <u>{organizer}</u> with course material provided by {org_course_material}. Passing an online exam, conducted remotely from IIT Bombay, is a pre-requisite for completing this training. <u>{invigilator}</u> at {institution} invigilated this examination.<br/>This training is offered by the Spoken Tutorial Project, IIT Bombay, funded by National Mission on Education through ICT, Ministry of Education, Govt., of India."
    else:
-      text = f"This is to certify that <b>{name}</b> has successfully completed <b>{foss}</b> test organized at <b>{institution}</b> by <b>{organizer}</b>  with course material provided by {organization}. Passing an online exam, conducted remotely from IIT Bombay, is a pre-requisite for completing this training. <br /><p><b>{invigilator}</b> from <b>{institution}</b> invigilated this examination. {text_end}.</p><br /><br />{credits}"
+      text = f"This is to certify that <b>{name}</b> has successfully completed <b>{foss}</b> test organized at <b>{institution}</b> by <b>{organizer}</b>  with course material provided by {org_course_material}. Passing an online exam, conducted remotely from IIT Bombay, is a pre-requisite for completing this training. <br /><p><b>{invigilator}</b> from <b>{institution}</b> invigilated this examination. {text_end}.</p><br /><br />{credits}"
    return text
